bf2bdcc2835b2c0549c720b28dd106ebe4c3c09a
flo <finkf@cis.uni-muenchen.de>
2016-06-15 16:08:14 +0200

    change default user's emails

--- misc/default/config.def.mk
+++ misc/default/config.def.mk
@@ -13,6 +13,6 @@ PCW_LOG_FILE="pocoweb.log"
 
 # default users
 # each entry: name,email,institute,passwd
-PCW_DEF_USERS += "flo,finkf@cis.lmu.de,CIS,flo"
-PCW_DEF_USERS += "uwe,springmann@cis.lmu.de,CIS,uwe"
-PCW_DEF_USERS += "klaus,schulz@cis.lmu.de,CIS,klaus"
+PCW_DEF_USERS += "flo,fake-email@example.org,CIS,flo"
+PCW_DEF_USERS += "uwe,fake-email@example.org,CIS,uwe"
+PCW_DEF_USERS += "klaus,fake-email@example.org,CIS,klaus"
