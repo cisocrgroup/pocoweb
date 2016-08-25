d88df1a75d88b6e17b8867e3ac6d3f4c6eb0e08a
flo <finkf@cis.uni-muenchen.de>
2016-08-25 15:45:40 +0200

    add store/load from database

--- rest/src/doc/Book.cpp
+++ rest/src/doc/Book.cpp
@@ -7,6 +7,9 @@
 #include "Book.hpp"
 
 ///////////////////////////////////////////////////////////////////////////////
+pcw::Book::Book(int iid): id(id) {assert(id > 0);}
+
+///////////////////////////////////////////////////////////////////////////////
 pcw::PagePtr 
 pcw::Book::add_page(PagePtr page)
 {
@@ -26,6 +29,52 @@ pcw::Book::get_page(int id) const noexcept
 		return (*this)[id - 1];
 	}
 	return nullptr;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void
+pcw::Book::store(sql::Connection& c) const
+{
+	static const auto *sql = "UPDATE books "
+				 "SET firstpage=?, lastpage=?,bookdataid=?"
+				 "WHERE bookid=?";
+	StatementPtr s{c.prepareStatement(sql)};
+	assert(s);
+	s->setInt(1, first_page_id());
+	s->setInt(2, last_page_id());
+	s->setInt(3, data.id);
+	s->setInt(3, id);
+	s->executeUpdate();	
+
+	data.store(c);
+	for (const auto& page: *this) {
+		if (page)
+			page->store(c, id);
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void
+pcw::Book::load(sql::Connection& c)
+{
+	static const auto *sql = "SELECT * FROM books WHERE bookid=?";
+	StatementPtr s{c.prepareStatement(sql)};
+	assert(s);
+	s->setInt(1, id);
+	ResultPtr res{s->executeQuery()};
+	if (not res or not res->next())
+		throw std::runtime_error("(Book) No such book id " + std::to_string(id));
+	
+	auto firstpage = res->getInt("firstpage");
+	auto lastpage = res->getInt("lastpage");
+	data.id = res->getInt("dataid");
+	
+	data.load(c);
+	resize(lastpage);
+	for (int i = firstpage; i > 0 and i <= lastpage; ++i) {
+		(*this)[i - 1] = std::make_shared<Page>(i);
+		(*this)[i - 1]->load(c, id);
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -50,4 +99,24 @@ pcw::Book::store(nlohmann::json& json) const
 		page->store(tmp);
 		json["pages"].push_back(tmp);
 	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+int 
+pcw::Book::first_page_id() const noexcept
+{
+	auto i = std::find_if(begin(*this), end(*this), [](const auto& p) {return p});
+	return i != end(*this) ? i->operator*().id : 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+int 
+pcw::Book::last_page_id() const noexcept
+{
+	auto i = std::find_if(
+		std::reverse_iterator(end(*this)),
+		std::reverse_iterator(begin(*this)),
+		[](const auto& page) {return page;}
+	);
+	return i != std::reverse_iterator(begin(*this)) ? i->operator*().id : 0;
 }
--- rest/src/doc/Book.hpp
+++ rest/src/doc/Book.hpp
@@ -4,19 +4,29 @@
 #include "Page.hpp"
 #include "BookData.hpp"
 
+namespace sql {
+	class Connection;
+}
+
 namespace pcw {
 	class Book: public std::enable_shared_from_this<Book>,
 		    public Container<Page> {
 	public:
-		Book() = default;
+		Book(int id);
 		Book(nlohmann::json& json): Book() {load(json);}
 		virtual ~Book() noexcept override = default;	
 		PagePtr add_page(PagePtr page);
 		PagePtr get_page(int id) const noexcept;
+		void store(sql::Connection& c) const;
+		void load(sql::Connection& c);
 		void load(nlohmann::json& json);
 		void store(nlohmann::json& json) const;
 		BookData data;
 		int id;
+	
+	private:
+		int first_page_id() const noexcept;
+		int last_page_id() const noexcept;
 	};
 	using BookPtr = std::shared_ptr<Book>;
 }
