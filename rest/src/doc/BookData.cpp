a5f8cdb3c724da0285f280d90b9d72291eb4e945
flo <finkf@cis.uni-muenchen.de>
2016-08-25 15:40:52 +0200

    implement store/load from database

--- rest/src/doc/BookData.cpp
+++ rest/src/doc/BookData.cpp
@@ -2,6 +2,47 @@
 #include "BookData.hpp"
 
 ///////////////////////////////////////////////////////////////////////////////
+void
+pcw::BookData::store(sql::Connection& c) const
+{
+	static const char *sql = "UPDATE bookdata "
+				 "SET owner=?,year=?,title=?,author=?,"
+				 "description=?,uri=?,directory=? "
+				 "WHERE bookdataid=?";
+	StatementPtr s{c.prepareStatement(sql)};
+	assert(s);
+	s->setInt(1, owner);
+	s->setInt(2, year);
+	s->setString(3, title);
+	s->setString(4, author);
+	s->setString(5, description);
+	s->setString(6, uri);
+	s->setString(7, path.string());
+	s->setInt(8, id);
+	s->executeUpdate();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void
+pcw::BookData::load(sql::Connection& c) 
+{
+	static const char *sql = "SELECT * FROM bookdata WHERE bookdataid=?";
+	StatementPtr s{c.prepareStatement(sql)};
+	assert(s);
+	s->setInt(1, id);
+	ResultPtr res{s->executeQuery()};
+	if (not res or not res->next()) 
+		throw std::runtime_error("(BookData) No such book data id " + std::to_string(id));
+	owner = s->getInt("owner");
+	year = s->getInt("year");
+	title = s->getString("title");
+	author = s->getString("author");
+	description = s->getString("description");
+	uri = s->getString("uri");
+	path = s->getString("directory");
+}
+
+///////////////////////////////////////////////////////////////////////////////
 void 
 pcw::BookData::load(nlohmann::json& json)
 {
--- rest/src/doc/BookData.hpp
+++ rest/src/doc/BookData.hpp
@@ -1,12 +1,19 @@
 #ifndef pcw_BookData_hpp__
 #define pcw_BookData_hpp__
 
+namespace sql {
+	class Connection;
+}
+
 namespace pcw {
 	struct BookData {
 		BookData() = default;
 		BookData(nlohmann::json& json): BookData() {load(json);}
 		std::string title, author, desc, uri, path;
 		int id, year, firstpage, lastpage, owner;
+		
+		void load(sql::Connection& c);
+		void store(sql::Connection& c) const;
 		void load(nlohmann::json& json);
 		void store(nlohmann::json& json) const;
 	};
