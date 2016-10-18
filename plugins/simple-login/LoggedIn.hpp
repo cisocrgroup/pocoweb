88da8f0704c98f4abda65901bd8f615876db30c3
flo <finkf@cis.uni-muenchen.de>
2016-10-18 18:07:47 +0200

    add LoggedIn route to check if you have successfully logged in with a valid user account

---
+++ plugins/simple-login/LoggedIn.hpp
@@ -0,0 +1,22 @@
+#ifndef pcw_LoggedIn_hpp__
+#define pcw_LoggedIn_hpp__
+
+#include "Route.hpp"
+
+namespace pcw {
+	class LoggedIn: public Route {
+	public:
+		virtual ~LoggedIn() noexcept override = default;
+		virtual const char *route() const noexcept override {return route_;}
+		virtual const char *name() const noexcept override {return name_;}
+		virtual void Register(App&) override;
+		crow::response operator()(const crow::request& request) const;
+
+	private:
+		static const char* route_;
+		static const char* name_;
+	};
+}
+
+#endif // pcw_LoggedIn_hpp__
+
