2ae8fcc40e140cc4aba4ffb3eedee562c79fa6be
flo <finkf@cis.uni-muenchen.de>
2016-10-19 12:28:55 +0200

    update test

--- test/simple-login-test.sh
+++ test/simple-login-test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-set -x
+#set -x
 
+verbose=""
 tdir=$(mktemp -d)
 
 function finish {
@@ -16,14 +17,20 @@ cookies=$tdir/cookies.txt
 cp misc/default/config.def.ini $config
 
 # start server
-host=$(git config -f $config --get daemon.host)
-port=$(git config -f $config --get daemon.port)
+host=$(git config -f $config --get daemon.host) || exit 1
+port=$(git config -f $config --get daemon.port) || exit 1
+user=$(git config -f $config --get plugin-simple-login.default-user) || exit 1
+pass=$(git config -f $config --get plugin-simple-login.default-pass) || exit 1
+
 rest/pcwd $config 2>/dev/null &
 pid=$!
 echo $pid > $tdir/pid
+sleep 1
 
 # -XGET is automatically inferred
-curl -c $cookies -v [$host]:$port/api-version
-curl -c $cookies -v [$host]:$port/login/user/$user/pass/$pass
+curl -s -o /dev/null -w '%{http_code}\n' [$host]:$port/api-version
+curl -s -o /dev/null -c $cookies $verbose -w '%{http_code}\n' [$host]:$port/login/user/$user/pass/$pass
+curl -s -o /dev/null -b $cookies $verbose -w '%{http_code}\n' [$host]:$port/logged-in
+curl -s -o /dev/null -b $cookies $verbose -w '%{http_code}\n' [$host]:$port/create-user/user/test/pass/test123
 #curl -XPOST -c /tmp/cookies.txt -v $host:$port/login/username/$name/password/$pass
 
