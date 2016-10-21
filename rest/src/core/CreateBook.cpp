8ca460998fe5701910836dccae438b2a269fd526
flo <finkf@cis.uni-muenchen.de>
2016-10-21 16:39:01 +0200

    add scope guard to automatically clean up book directories on failure;

--- rest/src/core/CreateBook.cpp
+++ rest/src/core/CreateBook.cpp
@@ -7,6 +7,7 @@
 #include "Database.hpp"
 #include "CreateBook.hpp"
 #include "Sessions.hpp"
+#include "ScopeGuard.hpp"
 
 #define CREATE_BOOK_ROUTE "/create/book/author/<string>/title/<string>"
 
@@ -30,7 +31,6 @@ CreateBook::operator()(
 	const std::string& author,
 	const std::string& title
 ) const {
-	BookPtr book = nullptr;
 	try {
 		auto db = database(request);
 		if (not db)
@@ -38,26 +38,24 @@ CreateBook::operator()(
 
 		std::lock_guard<std::mutex> lock(db->session().mutex);
 		db->set_autocommit(false);
-		book = db->insert_book(author, title);
+		auto book = db->insert_book(author, title);
 		if (not book) // should not happen
 			return internal_server_error();
 
+		ScopeGuard sg([&book](){assert(book); book->directory.remove();});
 		CROW_LOG_INFO << "(CreateBook) BookDir: " << book->directory.path();
 		book->directory.setup(extract_content(request), *book);
 		book->directory.clean_up();
 		db->insert_book_pages(*book);
 		db->session().current_book = book;
 		db->commit();
+		sg.dismiss();
 		return created();
 	} catch (const BadRequest& e) {
 		CROW_LOG_ERROR << "(CreateBook) Error: " << e.what();
-		if (book) 
-			book->directory.remove();
 		return bad_request();
 	} catch (const std::exception& e) {
 		CROW_LOG_ERROR << "(CreateBook) Error: " << e.what();
-		if (book)
-			book->directory.remove();
 		return internal_server_error();
 	}
 }
