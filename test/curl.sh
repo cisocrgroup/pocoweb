cb0aca0693a7ef1710747446a3d339c673bcc46b
flo <finkf@cis.uni-muenchen.de>
2016-10-26 12:03:43 +0200

    add output to curl

--- test/curl.sh
+++ test/curl.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 #set -x
+set -e
+#usage: $0 tmpdir expected-response-code args...
 
-#usage: $0 cookie-file expected-response-code args...
-cookie=$1
+cookie=$1/cookies.txt
+output=$1/output.txt
 shift
 code=$1
 shift
 
-res=$(curl -s -o /dev/null -b $cookie -c $cookie -w '%{http_code}' $@) || exit
+res=$(curl -o $output -s -b $cookie -c $cookie -w '%{http_code}' $@) || exit
 
-if [ ! $code = $res ]; then
+if [ $code = $res ]; then
+	echo $(cat $output)
+else
 	echo "[error] expected $code but got $res" 1>&2
 	exit 1
 fi
--- test/simple-login-test.sh
+++ test/simple-login-test.sh
@@ -32,20 +32,20 @@ fi;
 
 # -XGET is automatically inferred
 echo "/api-version?" 1>&2
-test/curl.sh $cookies 200 [$host]:$port/api-version
+test/curl.sh $tdir 200 [$host]:$port/api-version
 echo "/login/user/pocoweb?" 1>&2
-test/curl.sh $cookies 200 [$host]:$port/login/user/$user/pass/$pass
+test/curl.sh $tdir 200 [$host]:$port/login/user/$user/pass/$pass
 echo "/logged-in?" 1>&2
-test/curl.sh $cookies 200 [$host]:$port/logged-in
+test/curl.sh $tdir 200 [$host]:$port/logged-in
 echo "/create/user/test?" 1>&2
-test/curl.sh $cookies 201 [$host]:$port/create/user/test/pass/test123
+test/curl.sh $tdir 201 [$host]:$port/create/user/test/pass/test123
 echo "/login/user/test?" 1>&2
-test/curl.sh $cookies 200 [$host]:$port/login/user/test/pass/test123
+test/curl.sh $tdir 200 [$host]:$port/login/user/test/pass/test123
 echo "/logged-in?" 1>&2
-test/curl.sh $cookies 200 [$host]:$port/logged-in
+test/curl.sh $tdir 200 [$host]:$port/logged-in
 echo "/update/user/test/set/email?" 1>&2
-test/curl.sh $cookies 200 [$host]:$port/update/user/test/set/email/foo@bar
+test/curl.sh $tdir 200 [$host]:$port/update/user/test/set/email/foo@bar
 echo "/update/user/test/set/institute?" 1>&2
-test/curl.sh $cookies 200 [$host]:$port/update/user/test/set/institute/test
+test/curl.sh $tdir 200 [$host]:$port/update/user/test/set/institute/test
 echo "delete/user/test?" 1>&2
-test/curl.sh $cookies 200 [$host]:$port/delete/user/test
+test/curl.sh $tdir 200 [$host]:$port/delete/user/test
