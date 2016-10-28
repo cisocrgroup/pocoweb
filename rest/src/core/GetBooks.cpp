57dea76f0514d6cefd419cbf77b43d6a6b8aaa03
flo <finkf@cis.uni-muenchen.de>
2016-10-28 13:47:31 +0200

    put json printout functions into their own files

--- rest/src/core/GetBooks.cpp
+++ rest/src/core/GetBooks.cpp
@@ -1,6 +1,7 @@
 #include <cppconn/connection.h>
 #include <regex>
 #include <crow.h>
+#include "jsonify.hpp"
 #include "BadRequest.hpp"
 #include "User.hpp"
 #include "Page.hpp"
@@ -21,11 +22,6 @@
 
 
 using namespace pcw;
-
-static crow::json::wvalue project_to_json(const Project& project);
-static crow::json::wvalue page_to_json(const Page& page);
-static crow::json::wvalue line_to_json(const Line& line);
-static crow::json::wvalue box_to_json(const Box& box);
 
 ////////////////////////////////////////////////////////////////////////////////
 const char* GetBooks::route_ = 
@@ -64,11 +60,11 @@ GetBooks::operator()(const crow::request& req) const
 	auto projects = db->select_all_projects(*db->session().user);
 	CROW_LOG_DEBUG << "Loaded " << projects.size() << " projects";
 
-	crow::json::wvalue j;
+	Json j;
 	size_t i = 0;
 	for (const auto& p: projects) {
 		assert(p);
-		j["books"][i++] = project_to_json(*p);
+		j["books"][i++] << *p;
 	}
 	return j;
 }
@@ -102,7 +98,8 @@ GetBooks::operator()(const crow::request& req, int prid, int pageid) const
 	auto page = project->find(pageid);
 	if (not page)
 		return not_found();
-	return page_to_json(*page);
+	Json j;
+	return j << *page;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -124,7 +121,8 @@ GetBooks::get(const crow::request& req, int prid) const
 	if (not project)
 		return not_found();
 	// TODO missing authentication
-	return project_to_json(*project);
+	Json j;
+	return j << *project;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -138,6 +136,7 @@ GetBooks::post(const crow::request& req, int prid) const
 	if (not proj)
 		return not_found();
 	auto user = db->session().user;
+
 	if (not user)
 		return internal_server_error();	
 
@@ -159,73 +158,3 @@ GetBooks::post(const crow::request& req, int prid) const
 	return created();
 }
 
-////////////////////////////////////////////////////////////////////////////////
-crow::json::wvalue 
-project_to_json(const Project& project)
-{
-	crow::json::wvalue j;
-	j["id"] = project.origin().id();
-	j["uri"] = project.origin().uri;
-	j["author"] = project.origin().author;
-	j["title"] = project.origin().title;
-	j["year"] = project.origin().year;
-	j["description"] = project.origin().description;
-	
-	std::vector<int> ids;
-	ids.resize(project.size());
-	std::transform(begin(project), end(project), begin(ids), [](const auto& page) {
-		assert(page);
-		return page->id;
-	});
-	j["pages"] = ids.size();
-	j["pageIds"] = ids;
-	return j;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-crow::json::wvalue 
-page_to_json(const Page& page)
-{
-	crow::json::wvalue j;
-	j["id"] = page.id;
-	j["box"] = box_to_json(page.box);
-	j["ocrFile"] = page.ocr.native();
-	j["imgFile"] = page.img.native();
-	
-	//std::vector<crow::json::wvalue> lines;
-	//lines.reserve(page.size());
-	//std::transform(begin(page), end(page), begin(lines), line_to_json);	
-	size_t i = 0;
-	for (const auto& line: page)
-		j["lines"][i++] = line_to_json(line);
-	// j["lines"] = lines;
-	return j;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-crow::json::wvalue 
-line_to_json(const Line& line)
-{
-	crow::json::wvalue j;
-	j["id"] = line.id;
-	j["box"] = box_to_json(line.box);
-	j["imgFile"] = line.img.native();
-	j["str"] = line.string();
-	j["cuts"] = line.cuts();
-	j["confidences"] = line.confidences();
-	return j;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-crow::json::wvalue 
-box_to_json(const Box& box)
-{
-	crow::json::wvalue j;
-	j["left"] = box.left();
-	j["right"] = box.right();
-	j["top"] = box.top();
-	j["bottom"] = box.bottom();
-	j["width"] = box.width();
-	j["height"] = box.height();
-	return j;
-}
