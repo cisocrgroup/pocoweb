78f9715ec563a8813a0349e4a6422e6bb85041df
flo <finkf@cis.uni-muenchen.de>
2016-11-14 11:11:45 +0100

    implement operator==, operator< and operator<<

--- rest/src/core/Box.hpp
+++ rest/src/core/Box.hpp
@@ -2,6 +2,7 @@
 #define pcw_Box_hpp__
 
 #include <vector>
+#include <tuple>
 
 namespace pcw {
 	struct Box {
@@ -16,20 +17,39 @@ namespace pcw {
 		void set_bottom(int x) noexcept {bottom_ = x;}
 		int width() const noexcept {return right_ - left_;}
 		int height() const noexcept {return bottom_ - top_;}
-		
+
 		Box& increase_left(int d) noexcept;
 		Box& increase_right(int d) noexcept;
 		Box& increase_top(int d) noexcept;
 		Box& increase_bottom(int d) noexcept;
 		Box& increase(int d) noexcept;
-		
+
 		Box& operator+=(const Box& other);
-		
+
 		std::vector<Box> split(int n) const;
 
 	private:
 		int left_, top_, right_, bottom_;
 	};
+
+	static inline bool
+	operator==(const Box& a, const Box& b) noexcept {
+		return std::make_tuple(a.left(), a.top(), a.right(), a.bottom()) ==
+			std::make_tuple(b.left(), b.top(), b.right(), b.bottom());
+	}
+	static inline bool
+	operator<(const Box& a, const Box& b) noexcept {
+		return std::make_tuple(a.left(), a.top(), a.right(), a.bottom()) <
+			std::make_tuple(b.left(), b.top(), b.right(), b.bottom());
+	}
+
+	template<class C>
+	inline std::basic_ostream<C>&
+	operator<<(std::basic_ostream<C>& os, const Box& box) {
+		return os << "bbox "
+			  << box.left() << " " << box.top() << " "
+			  << box.right() << " " << box.bottom();
+	}
 }
 
 #endif // pcw_Box_hpp__
