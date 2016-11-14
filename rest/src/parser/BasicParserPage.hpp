2d7ff48e118afaeb9ce1987bc08b6398b0130bc5
flo <finkf@cis.uni-muenchen.de>
2016-11-14 15:52:00 +0100

    add basic parser page class

---
+++ rest/src/parser/BasicParserPage.hpp
@@ -0,0 +1,36 @@
+#ifndef pcw_BasicParserPage_hpp__
+#define pcw_BasicParserPage_hpp__
+
+#include <memory>
+#include <vector>
+#include "ParserPage.hpp"
+
+namespace pcw {
+	class BasicParserPage: public ParserPage {
+	public:
+		BasicParserPage() = default;
+		virtual ~BasicParserPage() noexcept override = default;
+		virtual size_t size() const noexcept override {
+			return lines_.size();
+		}
+		virtual ParserLine& get(size_t i) const override {
+			return *lines_[i];
+		}
+		virtual ParserLine& get(size_t i) override {
+			return *lines_[i];
+		}
+		const std::vector<ParserLinePtr>& lines() const noexcept {
+			return lines_;
+		}
+		std::vector<ParserLinePtr>& lines() noexcept {
+			return lines_;
+		}
+
+	private:
+		std::vector<ParserLinePtr> lines_;
+	};
+}
+
+#endif // pcw_BasicParserPage_hpp__
+
+
--- rest/src/parser/XmlParserPage.hpp
+++ rest/src/parser/XmlParserPage.hpp
@@ -2,40 +2,24 @@
 #define pcw_XmlParserPage_hpp__
 
 #include <memory>
-#include "ParserPage.hpp"
+#include "BasicParserPage.hpp"
 #include "Xml.hpp"
 
 namespace pcw {
-	class XmlParserPage: public ParserPage {
+	class XmlParserPage: public BasicParserPage {
 	public:
 		using Node = pugi::xml_node;
 		using Doc = pugi::xml_document;
 		using DocPtr = std::shared_ptr<Doc>;
 
-		XmlParserPage(Xml xml = {}): lines_(), xml_(xml) {}
-		XmlParserPage(const Path& path): lines_(), xml_(path) {}
+		XmlParserPage(Xml xml = {}): xml_(xml) {}
+		XmlParserPage(const Path& path): xml_(path) {}
 		virtual ~XmlParserPage() noexcept override = default;
 		virtual void write(const Path& path) const override;
-		virtual size_t size() const noexcept override {
-			return lines_.size();
-		}
-		virtual ParserLine& get(size_t i) const override {
-			return *lines_[i];
-		}
-		virtual ParserLine& get(size_t i) override {
-			return *lines_[i];
-		}
 		Xml& xml() noexcept {return xml_;}
 		const Xml& xml() const noexcept {return xml_;}
-		const std::vector<ParserLinePtr>& lines() const noexcept {
-			return lines_;
-		}
-		std::vector<ParserLinePtr>& lines() noexcept {
-			return lines_;
-		}
 
 	private:
-		std::vector<ParserLinePtr> lines_;
 		Xml xml_;
 	};
 }
