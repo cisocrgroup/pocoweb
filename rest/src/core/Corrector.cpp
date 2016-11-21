7fa9b86f5d145c91bf353bc9de7fe066b3d40ffd
flo <finkf@cis.uni-muenchen.de>
2016-11-21 15:21:52 +0100

    finish Corrector

--- rest/src/core/Corrector.cpp
+++ rest/src/core/Corrector.cpp
@@ -1,4 +1,5 @@
 #include <utf8.h>
+#include <crow/logging.h>
 #include "Line.hpp"
 #include "Page.hpp"
 #include "Project.hpp"
@@ -29,10 +30,119 @@ Corrector::correct(int pid, int lid, const std::string& str)
 
 ////////////////////////////////////////////////////////////////////////////////
 void
+Corrector::correct(int pid, int lid, const std::wregex& pat, const std::wstring repl)
+{
+	if (view_) {
+		auto page = view_->find(pid);
+		if (page) {
+			correct((*page)[lid], pat, repl);
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Corrector::correct(int pid, int lid, const std::wstring& pat, const std::wstring repl)
+{
+	correct(pid, lid, std::wregex{pat}, repl);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Corrector::correct(int pid, int lid, const std::string& pat, const std::string repl)
+{
+	std::wstring wpat, wrepl;
+	wpat.reserve(pat.size() * 2);
+	wrepl.reserve(repl.size() * 2);
+	utf8::utf8to32(begin(pat), end(pat), std::back_inserter(wpat));
+	utf8::utf8to32(begin(repl), end(repl), std::back_inserter(wrepl));
+	correct(pid, lid, std::wregex{wpat}, wrepl);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Corrector::correct(int pid, const std::wregex& pat, const std::wstring repl)
+{
+	if (view_) {
+		auto page = view_->find(pid);
+		if (page) {
+			for (auto& line: *page)
+				correct(line, pat, repl);
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Corrector::correct(int pid, const std::wstring& pat, const std::wstring repl)
+{
+	correct(pid, std::wregex{pat}, repl);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Corrector::correct(int pid, const std::string& pat, const std::string repl)
+{
+	std::wstring wpat, wrepl;
+	wpat.reserve(pat.size() * 2);
+	wrepl.reserve(repl.size() * 2);
+	utf8::utf8to32(begin(pat), end(pat), std::back_inserter(wpat));
+	utf8::utf8to32(begin(repl), end(repl), std::back_inserter(wrepl));
+	correct(pid, std::wregex{wpat}, wrepl);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Corrector::correct(const std::wregex& pat, const std::wstring repl)
+{
+	if (view_) {
+		for (const auto& page: *view_) {
+			if (page) {
+				for (auto& line: *page)
+					correct(line, pat, repl);
+			}
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Corrector::correct(const std::wstring& pat, const std::wstring repl)
+{
+	correct(std::wregex{pat}, repl);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Corrector::correct(const std::string& pat, const std::string repl)
+{
+	std::wstring wpat, wrepl;
+	wpat.reserve(pat.size() * 2);
+	wrepl.reserve(repl.size() * 2);
+	utf8::utf8to32(begin(pat), end(pat), std::back_inserter(wpat));
+	utf8::utf8to32(begin(repl), end(repl), std::back_inserter(wrepl));
+	correct(std::wregex{wpat}, wrepl);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
 Corrector::correct(Line& line, const std::wstring& gt, bool partial)
 {
 	wf_.set_ocr(line);
 	wf_.set_gt(gt);
 	wf_();
+	CROW_LOG_INFO << "(Corrector) correcting\n" << wf_;
 	wf_.correct(line, partial);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Corrector::correct(Line& line, const std::wregex& pat, const std::wstring& repl)
+{
+	wf_.clear();
+	wf_.set_ocr(line);
+	wf_.set_gt(pat, repl);
+	wf_();
+	CROW_LOG_INFO << "(Corrector) correcting\n" << wf_;
+	wf_.correct(line, true);
 }
--- rest/src/core/Corrector.hpp
+++ rest/src/core/Corrector.hpp
@@ -2,6 +2,7 @@
 #define pcw_Corrector_hpp__
 
 #include <memory>
+#include <regex>
 #include "WagnerFischer.hpp"
 
 namespace pcw {
@@ -19,9 +20,20 @@ namespace pcw {
 		void correct(int pid, int lid, const std::wstring& str);
 		void correct(int pid, int lid, const std::string& str);
 
+		void correct(int pid, int lid, const std::wregex& pat, const std::wstring repl);
+		void correct(int pid, int lid, const std::wstring& pat, const std::wstring repl);
+		void correct(int pid, int lid, const std::string& pat, const std::string repl);
+		void correct(int pid, const std::wregex& pat, const std::wstring repl);
+		void correct(int pid, const std::wstring& pat, const std::wstring repl);
+		void correct(int pid, const std::string& pat, const std::string repl);
+		void correct(const std::wregex& pat, const std::wstring repl);
+		void correct(const std::wstring& pat, const std::wstring repl);
+		void correct(const std::string& pat, const std::string repl);
+
 
 	private:
 		void correct(Line& line, const std::wstring& gt, bool partial);
+		void correct(Line& line, const std::wregex& pat, const std::wstring& repl);
 
 		WagnerFischer wf_;
 		ProjectPtr view_;
