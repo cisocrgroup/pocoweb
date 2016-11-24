4adefa5895168fc013e2f1b4dbf0af09e7cffd96
flo <finkf@cis.uni-muenchen.de>
2016-11-24 11:38:38 +0100

    using curl for login

--- rest/src/pcwc.cpp
+++ rest/src/pcwc.cpp
@@ -1,6 +1,7 @@
 #include <boost/variant.hpp>
 #include <iostream>
 #include <regex>
+#include <curl/curl.h>
 #include "core/Error.hpp"
 #include "core/TmpDir.hpp"
 
@@ -12,7 +13,7 @@ struct Address {
 
 struct CVersion {};
 struct CQuit {};
-struct CError {std::string line;};
+struct CError {std::string what;};
 
 using Command = boost::variant<
 	CVersion,
@@ -20,24 +21,134 @@ using Command = boost::variant<
 	CError
 >;
 
-struct Data {
-	Data(char* argv[])
-		: host(argv[1])
-		, user(argv[3])
-		, pass(argv[4])
-		, line()
-		, port(std::stoi(argv[2]))
-		, tmpdir()
-	{}
-	std::string host, user, pass, line;
-	int port;
+static char EBUF[CURL_ERROR_SIZE + 1];
+static size_t xwrite(char *ptr, size_t size, size_t nmemb, void *userdata);
+
+struct Ed: boost::static_visitor<void> {
+	Ed(int argc, char* argv[]);
+	~Ed() noexcept;
+
+	void login();
+	void operator()(const std::string& line);
+	void operator()(CVersion) const;
+	void operator()(CQuit);
+	void operator()(const CError& error) const;
+
+	static Command parse(const std::string& line);
+
+	std::string host, user, pass;
 	TmpDir tmpdir;
+	CURL* curl;
+	int port;
+	bool done;
 };
 
-static Command parse_command(const std::string& line);
-static Command parse_command(const char* line);
-static Data parse_args(int argc, char* argv[]);
-static int run(int argc, char* argv[]);
+////////////////////////////////////////////////////////////////////////////////
+Ed::Ed(int argc, char* argv[])
+	: host()
+	, user()
+	, pass()
+	, tmpdir()
+	, curl()
+	, port()
+	, done(false)
+{
+	if (argc != 5)
+		THROW(Error, "Usage: ", argv[0], " <host> <port> <user> <pass>");
+	if (std::stoi(argv[2]) <= 0)
+		THROW(Error, "Expceted port > 0, got: ", argv[2]);
+	host = argv[1];
+	port = std::stoi(argv[2]);
+	user = argv[3];
+	pass = argv[4];
+}
+
+////////////////////////////////////////////////////////////////////////////////
+Ed::~Ed() noexcept
+{
+	curl_easy_cleanup(curl);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Ed::login()
+{
+	curl = curl_easy_init();
+	if (not curl)
+		THROW(Error, "Could not initialize curl");
+	auto url = "http://" + host + "/login/username/" + user + "/password/" + pass;
+	curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, EBUF);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &xwrite);
+	curl_easy_setopt(curl, CURLOPT_PORT, port);
+	curl_easy_setopt(curl, CURLOPT_URL, url.data());
+	if (curl_easy_perform(curl) != CURLE_OK)
+		THROW(Error, "curl_easy_perform(): ", EBUF);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Ed::operator()(const std::string& line)
+{
+	auto command = parse(line);
+	boost::apply_visitor(*this, command);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Ed::operator()(CVersion) const
+{
+	std::cout << "VERSION\n";
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Ed::operator()(CQuit)
+{
+	done = true;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Ed::operator()(const CError& error) const
+{
+	std::cerr << "[error] " << error.what << "\n";
+}
+
+////////////////////////////////////////////////////////////////////////////////
+Command
+Ed::parse(const std::string& line)
+{
+	static const std::regex versionre{R"(:vers?i?o?n?\s*)"};
+	static const std::regex quitre{R"(:qu?i?t?\s*)"};
+	if (std::regex_match(line, versionre))
+		return CVersion{};
+	if (std::regex_match(line, quitre))
+		return CQuit{};
+	else
+		return CError{"Invalid command: " + line};
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+size_t
+xwrite(char *ptr, size_t size, size_t nmemb, void *userdata)
+{
+	std::cout.write(ptr, size * nmemb);
+	return size * nmemb;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+static int
+run(int argc, char* argv[])
+{
+	Ed ed{argc, argv};
+	ed.login();
+	std::string line;
+	while (not ed.done and std::getline(std::cin, line)) {
+		ed(line);
+	}
+	return EXIT_SUCCESS;
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 int
@@ -53,53 +164,3 @@ main(int argc, char* argv[])
 	return EXIT_FAILURE;
 }
 
-////////////////////////////////////////////////////////////////////////////////
-int
-run(int argc, char* argv[])
-{
-	auto data = parse_args(argc, argv);
-	while (std::getline(std::cin, data.line)) {
-		auto command = parse_command(data.line);
-		if (boost::get<CVersion>(&command)) {
-			std::cout << "VERSION\n";
-		} else if (boost::get<CQuit>(&command)) {
-			break;
-		} else {
-			std::cout << "[error] Invalid line: "
-				  << boost::get<CError>(command).line << "\n";
-		}
-	}
-	return EXIT_SUCCESS;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-Command
-parse_command(const std::string& line)
-{
-	return parse_command(line.data());
-}
-
-////////////////////////////////////////////////////////////////////////////////
-Command
-parse_command(const char* line)
-{
-	static const std::regex versionre{R"(:vers?i?o?n?\s*)"};
-	static const std::regex quitre{R"(:qu?i?t?\s*)"};
-	if (std::regex_match(line, versionre))
-		return CVersion{};
-	if (std::regex_match(line, quitre))
-		return CQuit{};
-	else
-		return CError{line};
-}
-
-////////////////////////////////////////////////////////////////////////////////
-Data
-parse_args(int argc, char* argv[])
-{
-	if (argc != 5)
-		THROW(Error, "Usage: ", argv[0], " <host> <port> <user> <pass>");
-	if (std::stoi(argv[2]) <= 0)
-		THROW(Error, "Expceted port > 0, got: ", argv[2]);
-	return Data{argv};
-}
