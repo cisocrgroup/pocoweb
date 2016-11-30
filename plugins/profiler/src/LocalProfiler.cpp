dd486c5077f18c01011f3acad657128ecc0590ae
flo <finkf@cis.uni-muenchen.de>
2016-11-30 12:06:12 +0100

    improve logging output

--- plugins/profiler/src/LocalProfiler.cpp
+++ plugins/profiler/src/LocalProfiler.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 #include <thread>
 #include <crow/logging.h>
+#include "core/Book.hpp"
 #include "core/Error.hpp"
 #include "core/Profile.hpp"
 #include "LocalProfiler.hpp"
@@ -13,7 +14,8 @@ LocalProfiler::do_profile()
 {
 	using namespace std::chrono_literals;
 	auto sleeping_time = 10s;
-	CROW_LOG_DEBUG << "(LocalProfiler) going to sleep for 10s";
+	CROW_LOG_INFO << "(LocalProfiler) Profiling book id: " << book().id();
 	std::this_thread::sleep_for(sleeping_time);
+	CROW_LOG_INFO << "(LocalProfiler) Done profiling book id: " << book().id();
 	THROW(pcw::NotImplemented, "Not implemented LocalProfiler::do_profile");
 }
