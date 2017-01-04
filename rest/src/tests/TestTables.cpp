429d4f470b7adbf388d250cf97e4ea050fde3538
flo <finkf@cis.uni-muenchen.de>
2017-01-04 15:29:47 +0100

    add test for tables in database

--- rest/src/tests/TestTables.cpp
+++ rest/src/tests/TestTables.cpp
@@ -4,23 +4,43 @@
 #include <boost/test/unit_test.hpp>
 #include <iostream>
 #include <sqlpp11/sqlpp11.h>
-#include <sqlpp11/container/connection.h>
+#include <sqlpp11/mysql/mysql.h>
+#include <sqlpp11/mysql/mysql.h>
 #include "core/Tables.hpp"
+#include "core/Config.hpp"
 
-namespace sql = sqlpp::container;
+using namespace sqlpp;
 using namespace pcw;
 
+struct TestDbConfig {
+	std::shared_ptr<mysql::connection_config> config;
+	TestDbConfig(): config(std::make_shared<mysql::connection_config>()) {
+		auto c = Config::load("config.ini");
+		config->user = c.db.user;
+		config->host = c.db.host;
+		config->database = c.db.db;
+		config->password = c.db.pass;
+		config->debug = true;
+	}
+};
+
 struct TableUsersFixture {
-	using Users = std::vector<Tables::Users::Struct>;
-	sql::connection<Users> db;
+	TestDbConfig config;
+	mysql::connection db;
 	Tables::Users::Table users;
-	TableUsersFixture(): db({}), users() {
-		db(sqlpp::insert_into(users).set(
-				users.userid = 1,
-				users.name = "first name"));
-		db(sqlpp::insert_into(users).set(
-				users.userid = 2,
-				users.name = "second name"));
+	TableUsersFixture(): config(), db(config.config) {
+		db(remove_from(users).where(users.name == "first name"));
+		db(remove_from(users).where(users.name == "second name"));
+		db(insert_into(users).set(users.name = "first name"));
+		db(insert_into(users).set(users.name = "second name"));
+	}
+	~TableUsersFixture() noexcept {
+		try {
+			db(remove_from(users).where(users.name == "first name"));
+			db(remove_from(users).where(users.name == "second name"));
+		} catch (...) {
+			BOOST_FAIL("SQL error while removing users");
+		}
 	}
 };
 
@@ -30,12 +50,12 @@ BOOST_FIXTURE_TEST_SUITE(TableUsers, TableUsersFixture)
 ////////////////////////////////////////////////////////////////////////////////
 BOOST_AUTO_TEST_CASE(FirstUser)
 {
-	/*
-	const auto& user = db(
-			sqlpp::select(users.userid).
+	/*auto res = db(select(users.userid, users.name).
 			from(users).
-			where(users.userid == 1));
-	*/
+			where(users.userid == 1));*/
+	auto user = db(select(users.userid).from(users).where(users.name == "first name"));
+	BOOST_REQUIRE(not user.empty());
+	// BOOST_CHECK_EQUAL(not user.front().name, "first name");
 }
 
 ////////////////////////////////////////////////////////////////////////////////
