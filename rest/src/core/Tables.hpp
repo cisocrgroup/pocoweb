40bd2c5380ca60707a2381ae06a05dbfe28818e2
flo <finkf@cis.uni-muenchen.de>
2017-01-04 15:28:25 +0100

    use macros

--- rest/src/core/Tables.hpp
+++ rest/src/core/Tables.hpp
@@ -4,21 +4,25 @@
 #include <sqlpp11/table.h>
 #include <sqlpp11/column_types.h>
 
-#define FOO(s, n, t) \
-	struct s {\
-		struct _alias_t {\
-			static constexpr const char _literal[] = "n";\
-			using _name_t = sqlpp::make_char_sequence<\
-				sizeof(_literal), _literal>;\
-			template<class T> struct _member_t {\
-				T n;\
-				T& operator()() noexcept\
-					{return n;}\
-				const T& operator()() const noexcept\
-					{return n;}\
-			};\
-		};\
-		using _traits = ::sqlpp::make_traits<t>;\
+#define table_column(name, tname, type) \
+	struct name { \
+		struct _alias_t { \
+			static constexpr const char _literal[] = #name; \
+			using _name_t = sqlpp::make_char_sequence< \
+				sizeof(_literal), _literal>; \
+			template<class T> struct _member_t { \
+				T tname; \
+				T& operator()() noexcept \
+					{return tname;} \
+				const T& operator()() const noexcept \
+					{return tname;} \
+			}; \
+		}; \
+		using _traits = ::sqlpp::make_traits<::sqlpp::type>; \
+		template<typename T> struct accesssor_t { \
+			static decltype(T::tname)& get(T& t) {return t.tname;} \
+			static const decltype(T::tname)& get(const T& t) {return t.tname;} \
+		}; \
 	};
 
 namespace pcw {
@@ -29,43 +33,9 @@ namespace pcw {
 					institute, passwd;*/
 				int userid;
 			};
-			struct Userid {
-				struct _alias_t {
-					static constexpr const char _literal[] = "userid";
-					using _name_t = sqlpp::make_char_sequence<
-						sizeof(_literal), _literal>;
-					template<class T> struct _member_t {
-						T userid;
-						T& operator()() noexcept
-							{return userid;}
-						const T& operator()() const noexcept
-							{return userid;}
-					};
-				};
-				using _traits = ::sqlpp::make_traits<::sqlpp::bigint>;
-				template<typename T> struct accesssor_t {
-					static decltype(T::userid)& get(T& t) {return t.userid;}
-					static const decltype(T::userid)& get(const T& t) {return t.userid;}
-				};
-			};
-
-			struct Name {
-				struct _alias_t {
-					static constexpr const char _literal[] =  "name";
-					using _name_t = sqlpp::make_char_sequence<sizeof(_literal), _literal>;
-					template<typename T> struct _member_t {
-						T name;
-						T& operator()() { return name; }
-						const T& operator()() const { return name; }
-					};
-				};
-				using _traits = ::sqlpp::make_traits<::sqlpp::varchar>;
-				template<typename T> struct accesssor_t {
-					static decltype(T::name)& get(T& t) {return t.name;}
-					static const decltype(T::name)& get(const T& t) {return t.name;}
-				};
-			};
-			struct Table: sqlpp::table_t<Table, Userid, Name> {
+			table_column(UserId, userid, bigint);
+			table_column(Name, name, varchar);
+			struct Table: sqlpp::table_t<Table, UserId, Name> {
 				using _value_t = sqlpp::no_value_t;
 				struct _alias_t {
 					static constexpr const char _literal[] =  "users";
