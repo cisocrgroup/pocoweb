5192539e8f73b9f826f80a9e2bb4c7d44f1106a2
flo <finkf@cis.uni-muenchen.de>
2017-01-05 12:30:03 +0100

    use mocking class instead of real mysql connection for testing

--- rest/src/tests/TestTables.cpp
+++ rest/src/tests/TestTables.cpp
@@ -4,62 +4,26 @@
 #include <boost/test/unit_test.hpp>
 #include <iostream>
 #include <sqlpp11/sqlpp11.h>
-#include <sqlpp11/mysql/mysql.h>
-#include <sqlpp11/mysql/mysql.h>
+#include "MockDb.h"
 #include "core/Tables.h"
 #include "core/Config.hpp"
 
 using namespace sqlpp;
 using namespace pcw;
 
-struct TestDbConfig {
-	std::shared_ptr<mysql::connection_config> config;
-	TestDbConfig(): config(std::make_shared<mysql::connection_config>()) {
-		auto c = Config::load("config.ini");
-		config->user = c.db.user;
-		config->host = c.db.host;
-		config->database = c.db.db;
-		config->password = c.db.pass;
-		config->debug = true;
-	}
-};
-
-struct TableUsersFixture {
-	TestDbConfig config;
-	mysql::connection db;
-	tables::Users users;
-	// Tables::Users::Table users;
-	TableUsersFixture(): config(), db(config.config) {
-		db(remove_from(users).where(users.name == "first name"));
-		db(insert_into(users).set(
-			users.name = "first name",
-			users.email = "first email",
-			users.institute = "first insitute",
-			users.passwd = "first password"
-		));
-	}
-	~TableUsersFixture() noexcept {
-		try {
-			db(remove_from(users).where(users.name == "first name"));
-		} catch (...) {
-			BOOST_FAIL("SQL error while removing users");
-		}
-	}
-};
-
 ////////////////////////////////////////////////////////////////////////////////
-BOOST_FIXTURE_TEST_SUITE(TableUsers, TableUsersFixture)
-
-////////////////////////////////////////////////////////////////////////////////
-BOOST_AUTO_TEST_CASE(FirstUser)
+BOOST_AUTO_TEST_CASE(InsertUser)
 {
-	/*auto res = db(select(users.userid, users.name).
-			from(users).
-			where(users.userid == 1));*/
-	auto user = db(select(users.userid).from(users).where(users.name == "first name"));
-	BOOST_REQUIRE(not user.empty());
-	// BOOST_CHECK_EQUAL(not user.front().name, "first name");
+	MockDb db;
+	db.expect("INSERT INTO users (name,email,institute,passwd) "
+		"VALUES('name','email','inst','passwd')");
+	tables::Users users;
+	auto stmt = insert_into(users).set(
+		users.name = "name",
+		users.email = "email",
+		users.institute = "inst",
+		users.passwd = "passwd"
+	);
+	db(stmt);
+	db.validate();
 }
-
-////////////////////////////////////////////////////////////////////////////////
-BOOST_AUTO_TEST_SUITE_END()
