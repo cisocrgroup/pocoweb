144514f609f564304a43de7c03825ac82360ef2a
flo <finkf@cis.uni-muenchen.de>
2017-01-10 21:18:22 +0100

    use thread save crypt functions

--- rest/src/core/Password.cpp
+++ rest/src/core/Password.cpp
@@ -1,15 +1,17 @@
+#include <iostream>
 #include <algorithm>
 #include <random>
 #include <unistd.h>
+#include <crypt.h>
 #include <system_error>
 #include "Password.hpp"
 
 #ifndef PCW_PASSWORD_SEED_SIZE
-#define PCW_PASSWORD_SEED_SIZE 10
+#define PCW_PASSWORD_SEED_SIZE 2
 #endif // PCW_PASSWORD_SEED_SIZE
 
 #ifndef PCW_PASSWORD_ALGORITHM
-#define PCW_PASSWORD_ALGORITHM "$2a$"
+#define PCW_PASSWORD_ALGORITHM "$6$"
 #endif // PCW_PASSWORD_ALGORITHM
 
 using namespace pcw;
@@ -54,17 +56,21 @@ get_hash(const std::string& salted_hash)
 Password
 Password::make(const std::string& password)
 {
-	static const char chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-		                    "abcdefghijklmnopqrstuvwxyz";
+	static const char salt_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		                         "abcdefghijklmnopqrstuvwxyz"
+				         "0123456789./";
 	std::random_device r;
 	std::default_random_engine e(r());
-	std::uniform_int_distribution<int> d(0, 51);
+	std::uniform_int_distribution<int> d(0, 63);
+
 	std::string salt = PCW_PASSWORD_ALGORITHM;
-
 	std::generate_n(std::back_inserter(salt), PCW_PASSWORD_SEED_SIZE,
-			[&](){return chars[d(e)];});
+			[&](){return salt_chars[d(e)];});
+	salt += '$';
 
-	auto salted_hash = crypt(password.data(), salt.data());
+	crypt_data data;
+	data.initialized = 0;
+	auto salted_hash = crypt_r(password.data(), salt.data(), &data);
 	if (not salted_hash)
 		throw std::system_error(errno, std::system_category(), "crypt");
 	return Password(salted_hash);
@@ -81,7 +87,9 @@ Password::Password(const std::string& salted_hash)
 bool
 Password::authenticate(const std::string& password) const
 {
-	auto tmp = crypt(password.data(), salt_.data());
+	crypt_data data;
+	data.initialized = 0;
+	auto tmp = crypt_r(password.data(), salt_.data(), &data);
 	return get_salt(tmp) == salt_ and get_hash(tmp) == hash_;
 }
 
