ab289f6b70649aa874c25e21627fcc47a9a88117
flo <finkf@cis.uni-muenchen.de>
2017-01-13 14:14:02 +0100

    add Fixture to unit tests

--- rest/src/core/tests/TestDatabase.cpp
+++ rest/src/core/tests/TestDatabase.cpp
@@ -13,10 +13,17 @@
 using namespace sqlpp;
 using namespace pcw;
 
+struct UsersFixture {
+	MockDb db;
+	UsersFixture(): db() {}
+};
+
+////////////////////////////////////////////////////////////////////////////////
+BOOST_FIXTURE_TEST_SUITE(Users, UsersFixture)
+
 ////////////////////////////////////////////////////////////////////////////////
 BOOST_AUTO_TEST_CASE(CreateUser)
 {
-	MockDb db;
 	db.expect(std::regex(R"(INSERT INTO users \(.+\) VALUES\(.+\))"));
 	auto user = create_user(db, "name", "password", "email", "institute");
 	BOOST_REQUIRE(user);
@@ -29,7 +36,6 @@ BOOST_AUTO_TEST_CASE(CreateUser)
 ////////////////////////////////////////////////////////////////////////////////
 BOOST_AUTO_TEST_CASE(LoginUser)
 {
-	MockDb db;
 	db.expect(std::regex(R"(SELECT .* FROM users WHERE \(users.name='name'\))"));
 	login_user(db, "name", "password");
 	db.validate();
@@ -38,7 +44,6 @@ BOOST_AUTO_TEST_CASE(LoginUser)
 ////////////////////////////////////////////////////////////////////////////////
 BOOST_AUTO_TEST_CASE(UpdateUser)
 {
-	MockDb db;
 	User user("name", "email", "institute", 42);
 	db.expect("UPDATE users SET email='email',institute='institute' "
 			"WHERE (users.userid=42)");
@@ -49,7 +54,6 @@ BOOST_AUTO_TEST_CASE(UpdateUser)
 ////////////////////////////////////////////////////////////////////////////////
 BOOST_AUTO_TEST_CASE(SelectUserByName)
 {
-	MockDb db;
 	db.expect(std::regex(R"(SELECT .* FROM users WHERE \(users.name='name'\))"));
 	select_user(db, "name");
 	db.validate();
@@ -58,7 +62,6 @@ BOOST_AUTO_TEST_CASE(SelectUserByName)
 ////////////////////////////////////////////////////////////////////////////////
 BOOST_AUTO_TEST_CASE(SelectUserById)
 {
-	MockDb db;
 	db.expect(std::regex(R"(SELECT .* FROM users WHERE \(users.userid=42\))"));
 	select_user(db, 42);
 	db.validate();
@@ -67,7 +70,6 @@ BOOST_AUTO_TEST_CASE(SelectUserById)
 ////////////////////////////////////////////////////////////////////////////////
 BOOST_AUTO_TEST_CASE(DeleteUserByName)
 {
-	MockDb db;
 	db.expect("DELETE FROM users WHERE (users.name='name')");
 	delete_user(db, "name");
 	db.validate();
@@ -76,9 +78,11 @@ BOOST_AUTO_TEST_CASE(DeleteUserByName)
 ////////////////////////////////////////////////////////////////////////////////
 BOOST_AUTO_TEST_CASE(DeleteUserById)
 {
-	MockDb db;
 	db.expect("DELETE FROM users WHERE (users.userid=42)");
 	delete_user(db, 42);
 	db.validate();
 }
+
+////////////////////////////////////////////////////////////////////////////////
+BOOST_AUTO_TEST_SUITE_END()
 
