bddc81e9a67242a4af7a65b412adb10ed4305b86
flo <finkf@cis.uni-muenchen.de>
2017-01-21 21:57:26 +0100

    rename do_get to get_impl

--- rest/src/core/Cache.hpp
+++ rest/src/core/Cache.hpp
@@ -17,7 +17,7 @@ namespace pcw {
 		using Base::end;
 		using Base::size;
 		using Base::empty;
-		
+
 		void put(value_type t);
 		template<class G>
 		value_type get(int id, G g);
@@ -27,7 +27,7 @@ namespace pcw {
 	private:
 		using Iterator = typename Base::iterator;
 		template<class F, class G>
-		value_type do_get(F f, G g);
+		value_type get_impl(F f, G g);
 		template<class G>
 		value_type update(Iterator i, G g);
 		std::mutex mutex_;
@@ -52,30 +52,30 @@ pcw::Cache<T>::put(value_type t)
 ////////////////////////////////////////////////////////////////////////////////
 template<class T>
 template<class G>
-typename pcw::Cache<T>::value_type 
+typename pcw::Cache<T>::value_type
 pcw::Cache<T>::get(int id, G g)
 {
 	auto gg = [id,g](){return g(id);};
 	auto f = [id](const value_type& t) {return t->id() == id;};
-	return do_get(f, gg);
+	return get_impl(f, gg);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 template<class T>
 template<class G>
-typename pcw::Cache<T>::value_type 
+typename pcw::Cache<T>::value_type
 pcw::Cache<T>::get(const std::string& name, G g)
 {
 	auto gg = [&name,g](){return g(name);};
 	auto f = [&name](const value_type& t) {return t->name == name;};
-	return do_get(f, gg);
+	return get_impl(f, gg);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 template<class T>
 template<class F, class G>
-typename pcw::Cache<T>::value_type 
-pcw::Cache<T>::do_get(F f, G g)
+typename pcw::Cache<T>::value_type
+pcw::Cache<T>::get_impl(F f, G g)
 {
 	auto i = end();
 	// just lock the cache while searching
@@ -91,7 +91,7 @@ pcw::Cache<T>::do_get(F f, G g)
 ////////////////////////////////////////////////////////////////////////////////
 template<class T>
 template<class G>
-typename pcw::Cache<T>::value_type 
+typename pcw::Cache<T>::value_type
 pcw::Cache<T>::update(Iterator i, G g)
 {
 	const auto b = begin();
