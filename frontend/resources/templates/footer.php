994f2d203b6bab1999ae75442910115bcb956112
flo <flo@cis.lmu.de>
2017-06-27 10:41:29 +0200

    update footer

--- frontend/resources/templates/footer.php
+++ frontend/resources/templates/footer.php
@@ -1,6 +1,12 @@
 <footer class="footer">
 	<div class="text-muted footer-line text-center">
-		Legal disclaimer.
+		<a href="#">Back to top <span class="glyphicon glyphicon-menu-up"/></a>
+	</div>
+	<div class="text-muted footer-line text-center">
+		Designed and built by me.
+	</div>
+	<div class="text-muted footer-line text-center">
+		Code licensed under Foobar. Documentation licensed under Barfoo.
 	</div>
 	<div class="text-muted footer-line text-center">
 		This website uses cookies.
