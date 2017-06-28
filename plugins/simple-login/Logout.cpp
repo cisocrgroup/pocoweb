88d05bdb0878e86ca6199b6104fff38915327948
flo <flo@cis.lmu.de>
2017-06-28 10:42:52 +0200

    implement lougout route

---
+++ plugins/simple-login/Logout.cpp
@@ -0,0 +1,78 @@
+#include <crow.h>
+#include "Logout.hpp"
+#include "core/Session.hpp"
+#include "core/jsonify.hpp"
+#include "database/Database.hpp"
+#include "utils/QueryParser.hpp"
+
+using namespace pcw;
+
+#define LOGIN_ROUTE "/logout"
+
+////////////////////////////////////////////////////////////////////////////////
+const char* Logout::route_ = LOGIN_ROUTE;
+
+////////////////////////////////////////////////////////////////////////////////
+const char* Logout::name_ = "Logout";
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Logout::Register(App& app)
+{
+	CROW_ROUTE(app, LOGIN_ROUTE).methods("GET"_method)(*this);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+Route::Response
+Logout::login(const Request& req, const std::string& name, const std::string& pass) const
+{
+	CROW_LOG_INFO << "(Logout) login attempt for user: " << name;
+
+	// check for existing session
+	auto session = find_session(req);
+	if (session) {
+		CROW_LOG_INFO << "(Logout) existing session sid: " << session->id();
+		return ok();
+	}
+
+	// create new session
+	auto conn = connection();
+	assert(conn);
+	auto user = select_user(conn.db(), name);
+	if (not user) {
+		CROW_LOG_ERROR << "(Logout) invalid user: " << name;
+		return forbidden();
+	}
+	if (not user->password.authenticate(pass)) {
+		CROW_LOG_ERROR << "(Logout) invalid password for user: " << name;
+	}
+
+	session = new_session(*user);
+	if (not session) {
+		CROW_LOG_ERROR << "(Logout) could not create new session";
+		return internal_server_error();
+	}
+	SessionLock lock(*session);
+	using namespace std::literals::chrono_literals;
+	session->set_expiration_date_from_now(24h);
+	auto response = ok();
+	session->set_cookies(response);
+	CROW_LOG_INFO << "(Logout) login successfull for user: " << name
+		      << ", sid: " << session->id();
+	return response;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+Route::Response
+Logout::impl(HttpGet, const Request& req) const
+{
+	auto session = find_session(req);
+	if (not session) {
+		CROW_LOG_ERROR << "(Logout) not logged in";
+		return bad_request();
+	}
+	SessionLock lock(*session);
+	delete_session(*session);
+	return ok();
+}
+
---
+++ plugins/simple-login/Logout.hpp
@@ -0,0 +1,31 @@
+#ifndef pcw_Logout_hpp__
+#define pcw_Logout_hpp__
+
+#include <array>
+#include <boost/optional.hpp>
+#include "core/CrtpRoute.hpp"
+
+namespace pcw {
+	class Logout: public CrtpRoute<Logout> {
+	public:
+		virtual ~Logout() noexcept override = default;
+		virtual const char *route() const noexcept override {return route_;}
+		virtual const char *name() const noexcept override {return name_;}
+		virtual void Register(App&) override;
+
+		pcw_crtp_route_def_impl__();
+		Response impl(HttpGet, const Request& request) const;
+
+	private:
+		Response login(
+			const Request& req,
+			const std::string& name,
+			const std::string& pass
+		) const;
+		static const char* route_;
+		static const char* name_;
+	};
+}
+
+#endif // pcw_Logout_hpp__
+
