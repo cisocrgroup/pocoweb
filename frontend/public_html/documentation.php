0a6215ed3be9710447464371819e22ace1c414ab
flo <flo@cis.lmu.de>
2017-07-05 11:43:29 +0200

    add basic layout for documentation

--- frontend/public_html/documentation.php
+++ frontend/public_html/documentation.php
@@ -2,8 +2,43 @@
 require_once("../resources/config.php");
 require_once(TEMPLATES_PATH . "/header.php");
 ?>
-<h1>Documentation</h1>
+<div class="col-md-9">
+<!-- documentation -->
+<h1 class="page-header" id="documentation">Documentation</h1>
 <p><span class="glyphicon glyphicon-ban-circle"/>&nbsp;Coming soon...</p>
+<!-- User management -->
+<h2 class="page-header" id="user-management">User management</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vestibulum justo vitae imperdiet varius. Fusce finibus nisl lectus, a scelerisque erat sollicitudin ut. Ut accumsan tellus sit amet volutpat accumsan. Nunc sit amet leo ante. Proin turpis orci, ultricies mattis porttitor eu, cursus quis ligula. Suspendisse tincidunt ac sem ultrices imperdiet. Fusce quam tellus, ullamcorper sit amet erat id, sagittis feugiat erat.</p>
+<p>Nam ac mollis quam. Maecenas ornare molestie nunc egestas consectetur. In egestas, mi ut pharetra gravida, tortor est semper dolor, vitae dictum lacus lorem sed enim. Nullam ornare vitae velit fermentum interdum. Morbi eu ex purus. Aenean mi arcu, tincidunt et dolor sit amet, imperdiet dignissim erat. Curabitur in purus nec metus tincidunt gravida. Donec eu felis a arcu tempus facilisis. Etiam hendrerit, lectus ullamcorper pulvinar fringilla, est ligula cursus dui, sit amet pretium est leo nec dui. Integer ligula ipsum, tincidunt non fringilla vel, sollicitudin non turpis.</p>
+<p>Duis sit amet viverra risus. Duis tristique porttitor ex, vel ullamcorper nunc vulputate a. Duis ac sodales mi. Mauris condimentum erat at odio viverra, vitae maximus sem placerat. Aliquam non diam porttitor, egestas enim a, consequat turpis. Sed condimentum vulputate est, ut eleifend enim dictum non. Aliquam a venenatis risus. Nulla congue id ex et vehicula.</p>
+<p>Suspendisse ac odio at velit cursus vehicula placerat vel dui. Quisque luctus dapibus ante ac efficitur. Duis convallis auctor nisi, quis commodo dolor dictum eget. Phasellus maximus elit ante, ut placerat orci dictum nec. Vestibulum ornare aliquam metus, vel placerat tortor. Sed euismod ex vel nisl posuere tempor. Mauris ut nisi venenatis, aliquet diam at, vulputate libero. Donec vulputate, elit dictum mattis dapibus, ligula ante varius velit, vel ornare elit leo ut felis. Integer neque ante, feugiat id molestie eu, sagittis eu augue. Vivamus fringilla ligula nec finibus imperdiet. Maecenas vestibulum lorem eget felis mattis ornare. Proin pulvinar in tellus eget ornare.</p>
+<p>Nunc vestibulum tellus purus, ac accumsan risus consectetur id. Ut nibh erat, faucibus eget viverra sit amet, congue quis tortor. Etiam ornare vehicula accumsan. Curabitur consectetur rhoncus lorem eu scelerisque. Nam ipsum urna, rutrum sed aliquet at, pretium et tellus. Pellentesque eu hendrerit augue, ut condimentum sapien. Duis maximus, orci blandit placerat auctor, nibh neque fringilla lacus, quis viverra velit ipsum at quam. Donec vehicula sagittis ornare. Fusce quis suscipit quam, eget auctor ipsum. Morbi aliquam eu nulla vel volutpat.</p>
+<!-- Project management -->
+<h2 class="page-header" id="project-management">Project management</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vestibulum justo vitae imperdiet varius. Fusce finibus nisl lectus, a scelerisque erat sollicitudin ut. Ut accumsan tellus sit amet volutpat accumsan. Nunc sit amet leo ante. Proin turpis orci, ultricies mattis porttitor eu, cursus quis ligula. Suspendisse tincidunt ac sem ultrices imperdiet. Fusce quam tellus, ullamcorper sit amet erat id, sagittis feugiat erat.</p>
+<p>Nam ac mollis quam. Maecenas ornare molestie nunc egestas consectetur. In egestas, mi ut pharetra gravida, tortor est semper dolor, vitae dictum lacus lorem sed enim. Nullam ornare vitae velit fermentum interdum. Morbi eu ex purus. Aenean mi arcu, tincidunt et dolor sit amet, imperdiet dignissim erat. Curabitur in purus nec metus tincidunt gravida. Donec eu felis a arcu tempus facilisis. Etiam hendrerit, lectus ullamcorper pulvinar fringilla, est ligula cursus dui, sit amet pretium est leo nec dui. Integer ligula ipsum, tincidunt non fringilla vel, sollicitudin non turpis.</p>
+<p>Duis sit amet viverra risus. Duis tristique porttitor ex, vel ullamcorper nunc vulputate a. Duis ac sodales mi. Mauris condimentum erat at odio viverra, vitae maximus sem placerat. Aliquam non diam porttitor, egestas enim a, consequat turpis. Sed condimentum vulputate est, ut eleifend enim dictum non. Aliquam a venenatis risus. Nulla congue id ex et vehicula.</p>
+<p>Suspendisse ac odio at velit cursus vehicula placerat vel dui. Quisque luctus dapibus ante ac efficitur. Duis convallis auctor nisi, quis commodo dolor dictum eget. Phasellus maximus elit ante, ut placerat orci dictum nec. Vestibulum ornare aliquam metus, vel placerat tortor. Sed euismod ex vel nisl posuere tempor. Mauris ut nisi venenatis, aliquet diam at, vulputate libero. Donec vulputate, elit dictum mattis dapibus, ligula ante varius velit, vel ornare elit leo ut felis. Integer neque ante, feugiat id molestie eu, sagittis eu augue. Vivamus fringilla ligula nec finibus imperdiet. Maecenas vestibulum lorem eget felis mattis ornare. Proin pulvinar in tellus eget ornare.</p>
+<p>Nunc vestibulum tellus purus, ac accumsan risus consectetur id. Ut nibh erat, faucibus eget viverra sit amet, congue quis tortor. Etiam ornare vehicula accumsan. Curabitur consectetur rhoncus lorem eu scelerisque. Nam ipsum urna, rutrum sed aliquet at, pretium et tellus. Pellentesque eu hendrerit augue, ut condimentum sapien. Duis maximus, orci blandit placerat auctor, nibh neque fringilla lacus, quis viverra velit ipsum at quam. Donec vehicula sagittis ornare. Fusce quis suscipit quam, eget auctor ipsum. Morbi aliquam eu nulla vel volutpat.</p>
+<!-- Rest API -->
+<h2 class="page-header" id="rest-api">Rest API</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vestibulum justo vitae imperdiet varius. Fusce finibus nisl lectus, a scelerisque erat sollicitudin ut. Ut accumsan tellus sit amet volutpat accumsan. Nunc sit amet leo ante. Proin turpis orci, ultricies mattis porttitor eu, cursus quis ligula. Suspendisse tincidunt ac sem ultrices imperdiet. Fusce quam tellus, ullamcorper sit amet erat id, sagittis feugiat erat.</p>
+<p>Nam ac mollis quam. Maecenas ornare molestie nunc egestas consectetur. In egestas, mi ut pharetra gravida, tortor est semper dolor, vitae dictum lacus lorem sed enim. Nullam ornare vitae velit fermentum interdum. Morbi eu ex purus. Aenean mi arcu, tincidunt et dolor sit amet, imperdiet dignissim erat. Curabitur in purus nec metus tincidunt gravida. Donec eu felis a arcu tempus facilisis. Etiam hendrerit, lectus ullamcorper pulvinar fringilla, est ligula cursus dui, sit amet pretium est leo nec dui. Integer ligula ipsum, tincidunt non fringilla vel, sollicitudin non turpis.</p>
+<p>Duis sit amet viverra risus. Duis tristique porttitor ex, vel ullamcorper nunc vulputate a. Duis ac sodales mi. Mauris condimentum erat at odio viverra, vitae maximus sem placerat. Aliquam non diam porttitor, egestas enim a, consequat turpis. Sed condimentum vulputate est, ut eleifend enim dictum non. Aliquam a venenatis risus. Nulla congue id ex et vehicula.</p>
+<p>Suspendisse ac odio at velit cursus vehicula placerat vel dui. Quisque luctus dapibus ante ac efficitur. Duis convallis auctor nisi, quis commodo dolor dictum eget. Phasellus maximus elit ante, ut placerat orci dictum nec. Vestibulum ornare aliquam metus, vel placerat tortor. Sed euismod ex vel nisl posuere tempor. Mauris ut nisi venenatis, aliquet diam at, vulputate libero. Donec vulputate, elit dictum mattis dapibus, ligula ante varius velit, vel ornare elit leo ut felis. Integer neque ante, feugiat id molestie eu, sagittis eu augue. Vivamus fringilla ligula nec finibus imperdiet. Maecenas vestibulum lorem eget felis mattis ornare. Proin pulvinar in tellus eget ornare.</p>
+<p>Nunc vestibulum tellus purus, ac accumsan risus consectetur id. Ut nibh erat, faucibus eget viverra sit amet, congue quis tortor. Etiam ornare vehicula accumsan. Curabitur consectetur rhoncus lorem eu scelerisque. Nam ipsum urna, rutrum sed aliquet at, pretium et tellus. Pellentesque eu hendrerit augue, ut condimentum sapien. Duis maximus, orci blandit placerat auctor, nibh neque fringilla lacus, quis viverra velit ipsum at quam. Donec vehicula sagittis ornare. Fusce quis suscipit quam, eget auctor ipsum. Morbi aliquam eu nulla vel volutpat.</p>
+</div>
+<div class="col-md-3">
+<!-- navigation -->
+<nav class="bs-docs-sidebar hidden-print hidden-sm hidden-xs affix">
+<ul class="nav bs-docs-sidenav">
+<li class><a href="#documentation">Documentation</a></li>
+<li class><a href="#user-management">User management</a></li>
+<li class><a href="#project-management">Project management</a></li>
+<li class><a href="#rest-api">Rest API</a></li>
+</ul>
+</nav>
+</div>
 <?php
     require_once(TEMPLATES_PATH . "/footer.php");
 ?>
