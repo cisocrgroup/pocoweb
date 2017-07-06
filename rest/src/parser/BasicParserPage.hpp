f0763cb9e55bf69540fde837f2a90362665589eb
flo <flo@cis.lmu.de>
2017-07-06 13:36:20 +0200

    indentation

--- rest/src/parser/BasicParserPage.hpp
+++ rest/src/parser/BasicParserPage.hpp
@@ -6,31 +6,23 @@
 #include "ParserPage.hpp"
 
 namespace pcw {
-	class BasicParserPage: public ParserPage {
-	public:
-		BasicParserPage() = default;
-		virtual ~BasicParserPage() noexcept override = default;
-		virtual size_t size() const noexcept override {
-			return lines_.size();
-		}
-		virtual const ParserLine& get(size_t i) const override {
-			return *lines_[i];
-		}
-		virtual ParserLine& get(size_t i) override {
-			return *lines_[i];
-		}
-		const std::vector<ParserLinePtr>& lines() const noexcept {
-			return lines_;
-		}
-		std::vector<ParserLinePtr>& lines() noexcept {
-			return lines_;
-		}
+class BasicParserPage : public ParserPage {
+       public:
+	BasicParserPage() = default;
+	virtual ~BasicParserPage() noexcept override = default;
+	virtual size_t size() const noexcept override { return lines_.size(); }
+	virtual const ParserLine& get(size_t i) const override {
+		return *lines_[i];
+	}
+	virtual ParserLine& get(size_t i) override { return *lines_[i]; }
+	const std::vector<ParserLinePtr>& lines() const noexcept {
+		return lines_;
+	}
+	std::vector<ParserLinePtr>& lines() noexcept { return lines_; }
 
-	private:
-		std::vector<ParserLinePtr> lines_;
-	};
+       private:
+	std::vector<ParserLinePtr> lines_;
+};
 }
 
-#endif // pcw_BasicParserPage_hpp__
-
-
+#endif  // pcw_BasicParserPage_hpp__
