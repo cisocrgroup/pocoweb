3c5cd0423e723d075acffd1505fbaf1cab7e6844
flo <flo@cis.lmu.de>
2017-07-07 10:44:51 +0200

    build all needed directories for xml output

--- rest/src/parser/XmlParserPage.cpp
+++ rest/src/parser/XmlParserPage.cpp
@@ -1,12 +1,14 @@
+#include "XmlParserPage.hpp"
+#include <crow/logging.h>
+#include <boost/filesystem/operations.hpp>
 #include <stdexcept>
 #include "pugixml.hpp"
-#include "XmlParserPage.hpp"
 
 using namespace pcw;
 
 ////////////////////////////////////////////////////////////////////////////////
-void
-XmlParserPage::write(const Path& path) const
-{
+void XmlParserPage::write(const Path& path) const {
+	CROW_LOG_DEBUG << "(XmlParserPage) writing xml to " << path;
+	boost::filesystem::create_directories(path.parent_path());
 	xml_.write(path);
 }
