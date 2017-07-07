e4309341ba9cd051ec6d2b68db983eeea9f8ca68
flo <flo@cis.lmu.de>
2017-07-07 15:30:14 +0200

    indentation

--- plugins/simple-login/Logout.cpp
+++ plugins/simple-login/Logout.cpp
@@ -1,5 +1,5 @@
-#include <crow.h>
 #include "Logout.hpp"
+#include <crow.h>
 #include "core/Session.hpp"
 #include "core/jsonify.hpp"
 #include "database/Database.hpp"
@@ -15,22 +15,20 @@ const char* Logout::route_ = LOGIN_ROUTE;
 const char* Logout::name_ = "Logout";
 
 ////////////////////////////////////////////////////////////////////////////////
-void
-Logout::Register(App& app)
-{
+void Logout::Register(App& app) {
 	CROW_ROUTE(app, LOGIN_ROUTE).methods("GET"_method)(*this);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response
-Logout::login(const Request& req, const std::string& name, const std::string& pass) const
-{
+Route::Response Logout::login(const Request& req, const std::string& name,
+			      const std::string& pass) const {
 	CROW_LOG_INFO << "(Logout) login attempt for user: " << name;
 
 	// check for existing session
 	auto session = find_session(req);
 	if (session) {
-		CROW_LOG_INFO << "(Logout) existing session sid: " << session->id();
+		CROW_LOG_INFO << "(Logout) existing session sid: "
+			      << session->id();
 		return ok();
 	}
 
@@ -43,7 +41,8 @@ Logout::login(const Request& req, const std::string& name, const std::string& pa
 		return forbidden();
 	}
 	if (not user->password.authenticate(pass)) {
-		CROW_LOG_ERROR << "(Logout) invalid password for user: " << name;
+		CROW_LOG_ERROR << "(Logout) invalid password for user: "
+			       << name;
 	}
 
 	session = new_session(*user);
@@ -62,9 +61,7 @@ Logout::login(const Request& req, const std::string& name, const std::string& pa
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response
-Logout::impl(HttpGet, const Request& req) const
-{
+Route::Response Logout::impl(HttpGet, const Request& req) const {
 	auto session = find_session(req);
 	if (not session) {
 		CROW_LOG_ERROR << "(Logout) not logged in";
@@ -74,4 +71,3 @@ Logout::impl(HttpGet, const Request& req) const
 	delete_session(*session);
 	return ok();
 }
-
