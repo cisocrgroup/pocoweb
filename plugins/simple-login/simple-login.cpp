b3468fc8698134ce7e4e564a306b74b95c7511de
flo <flo@cis.lmu.de>
2017-07-07 09:36:39 +0200

    make the default user an administrator

--- plugins/simple-login/simple-login.cpp
+++ plugins/simple-login/simple-login.cpp
@@ -1,22 +1,20 @@
 #include <crow.h>
-#include "core/util.hpp"
-#include "core/App.hpp"
-#include "core/Config.hpp"
+#include "DeleteUser.hpp"
 #include "Login.hpp"
 #include "Logout.hpp"
-#include "core/Session.hpp"
-#include "core/Password.hpp"
-#include "database/Database.hpp"
 #include "UpdateUser.hpp"
-#include "DeleteUser.hpp"
+#include "core/App.hpp"
+#include "core/Config.hpp"
+#include "core/Password.hpp"
+#include "core/Session.hpp"
 #include "core/User.hpp"
+#include "core/util.hpp"
+#include "database/Database.hpp"
 
 using namespace pcw;
 
 ////////////////////////////////////////////////////////////////////////////////
-static void
-insert_default_user(const std::string& p, const App& app)
-{
+static void insert_default_user(const std::string& p, const App& app) {
 	auto conn = app.connection_pool().get_connection();
 	assert(conn);
 	auto name = app.config().plugins[p].get<std::string>("default-user");
@@ -24,23 +22,19 @@ insert_default_user(const std::string& p, const App& app)
 	if (not user) {
 		MysqlCommiter commiter(conn);
 		user = create_user(
-			conn.db(),
-			name,
-			app.config().plugins[p].get<std::string>("default-pass"),
-			app.config().plugins[p].get<std::string>("default-email"),
-			app.config().plugins[p].get<std::string>("default-institute")
-		);
-		if (not user)
-			THROW(Error, "could not create default user");
+		    conn.db(), name,
+		    app.config().plugins[p].get<std::string>("default-pass"),
+		    app.config().plugins[p].get<std::string>("default-email"),
+		    app.config().plugins[p].get<std::string>(
+			"default-institute"),
+		    true);
+		if (not user) THROW(Error, "could not create default user");
 		commiter.commit();
 	}
-
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-static const char*
-do_plugin(const std::string& p, App& app) noexcept
-{
+static const char* do_plugin(const std::string& p, App& app) noexcept {
 	try {
 		app.config().setup_logging();
 		insert_default_user(p, app);
@@ -63,10 +57,5 @@ do_plugin(const std::string& p, App& app) noexcept
 
 extern "C" {
 ////////////////////////////////////////////////////////////////////////////////
-const char*
-plugin(const std::string& p, App& app)
-{
-	return do_plugin(p, app);
+const char* plugin(const std::string& p, App& app) { return do_plugin(p, app); }
 }
-}
-
