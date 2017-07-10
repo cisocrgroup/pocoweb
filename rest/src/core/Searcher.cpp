e5f3cd604c151d062e779aea427c93c4da4c4fc5
flo <flo@cis.lmu.de>
2017-07-10 14:01:12 +0200

    indentation

--- rest/src/core/Searcher.cpp
+++ rest/src/core/Searcher.cpp
@@ -1,27 +1,23 @@
-#include <regex>
-#include <utf8.h>
-#include "Project.hpp"
 #include "Searcher.hpp"
+#include <utf8.h>
+#include <regex>
+#include "Project.hpp"
 
 using namespace pcw;
 
 ////////////////////////////////////////////////////////////////////////////////
-Searcher::Searcher(const Project& project): Searcher(project.shared_from_this()) {}
+Searcher::Searcher(const Project& project)
+    : Searcher(project.shared_from_this()) {}
 
 ////////////////////////////////////////////////////////////////////////////////
-void
-Searcher::set_project(const Project& project) noexcept
-{
+void Searcher::set_project(const Project& project) noexcept {
 	project_ = project.shared_from_this();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-std::vector<LineSptr>
-Searcher::find(const std::wstring& str) const
-{
+std::vector<LineSptr> Searcher::find(const std::wstring& str) const {
 	auto flags = std::regex::optimize | std::regex::nosubs;
-	if (ignore_case_)
-		flags |= std::regex::icase;
+	if (ignore_case_) flags |= std::regex::icase;
 	std::wregex re;
 	if (match_words_)
 		re = std::wregex(LR"(\b)" + str + LR"(\b)", flags);
@@ -33,12 +29,9 @@ Searcher::find(const std::wstring& str) const
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-std::vector<LineSptr>
-Searcher::find(const std::string& str) const
-{
+std::vector<LineSptr> Searcher::find(const std::string& str) const {
 	std::wstring wstr;
 	wstr.reserve(str.size());
 	utf8::utf8to32(begin(str), end(str), std::back_inserter(wstr));
 	return find(wstr);
 }
-
--- rest/src/core/Searcher.hpp
+++ rest/src/core/Searcher.hpp
@@ -3,71 +3,52 @@
 
 #include <memory>
 #include <vector>
-#include "Project.hpp"
-#include "Page.hpp"
 #include "Line.hpp"
+#include "Page.hpp"
+#include "Project.hpp"
 
 namespace pcw {
-	using LineSptr = std::shared_ptr<Line>;
-	using ProjectSptr = std::shared_ptr<Project>;
+using LineSptr = std::shared_ptr<Line>;
+using ProjectSptr = std::shared_ptr<Project>;
 
-	class Searcher {
-	public:
-		Searcher(): Searcher(nullptr) {}
-		Searcher(const Project& project);
+class Searcher {
+       public:
+	Searcher() : Searcher(nullptr) {}
+	Searcher(const Project& project);
 
-		void set_project(const Project& project) noexcept;
-		bool match_words() const noexcept
-		{
-			return match_words_;
-		}
-		void set_match_words(bool m=true) noexcept
-		{
-			match_words_ = m;
-		}
-		bool ignore_case() const noexcept
-		{
-			return ignore_case_;
-		}
-		void set_ignore_case(bool i=true) noexcept
-		{
-			ignore_case_ = i;
-		}
+	void set_project(const Project& project) noexcept;
+	bool match_words() const noexcept { return match_words_; }
+	void set_match_words(bool m = true) noexcept { match_words_ = m; }
+	bool ignore_case() const noexcept { return ignore_case_; }
+	void set_ignore_case(bool i = true) noexcept { ignore_case_ = i; }
 
-		std::vector<LineSptr> find(const std::wstring& str) const;
-		std::vector<LineSptr> find(const std::string& str) const;
+	std::vector<LineSptr> find(const std::wstring& str) const;
+	std::vector<LineSptr> find(const std::string& str) const;
 
-		template<class F>
-		std::vector<LineSptr> find_impl(F f) const;
+	template <class F>
+	std::vector<LineSptr> find_impl(F f) const;
 
-	private:
-		Searcher(std::shared_ptr<const Project> p)
-			: project_(std::move(p))
-		  	, match_words_(true)
-		  	, ignore_case_(true)
-		{}
-		std::shared_ptr<const Project> project_;
-		bool match_words_, ignore_case_;
-	};
+       private:
+	Searcher(std::shared_ptr<const Project> p)
+	    : project_(std::move(p)), match_words_(true), ignore_case_(true) {}
+	std::shared_ptr<const Project> project_;
+	bool match_words_, ignore_case_;
+};
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-template<class F>
-inline std::vector<pcw::LineSptr>
-pcw::Searcher::find_impl(F f) const
-{
-	if (not project_)
-		return {};
+template <class F>
+inline std::vector<pcw::LineSptr> pcw::Searcher::find_impl(F f) const {
+	if (not project_) return {};
 	std::vector<pcw::LineSptr> res;
-	for (const auto& page: *project_) {
+	for (const auto& page : *project_) {
 		if (page) {
-			for (const auto& line: *page) {
-				if (line and f(*line))
-					res.push_back(line);
+			for (const auto& line : *page) {
+				if (line and f(*line)) res.push_back(line);
 			}
 		}
 	}
 	return res;
 }
 
-#endif // pcw_Searcher_hpp__
+#endif  // pcw_Searcher_hpp__
