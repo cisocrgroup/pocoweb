47fa92bbc1617ceec89f6cb767bc39a767d71d36
flo <flo@cis.lmu.de>
2017-07-14 10:14:58 +0200

    indentation

--- rest/src/api/LineRoute.cpp
+++ rest/src/api/LineRoute.cpp
@@ -1,8 +1,8 @@
-#include <regex>
+#include "LineRoute.hpp"
 #include <crow.h>
 #include <unicode/uchar.h>
 #include <utf8.h>
-#include "LineRoute.hpp"
+#include <regex>
 #include "core/Book.hpp"
 #include "core/BookDirectoryBuilder.hpp"
 #include "core/Package.hpp"
@@ -23,16 +23,14 @@ const char* LineRoute::route_ = LINE_ROUTE_ROUTE;
 const char* LineRoute::name_ = "LineRoute";
 
 ////////////////////////////////////////////////////////////////////////////////
-void
-LineRoute::Register(App& app)
-{
-	CROW_ROUTE(app, LINE_ROUTE_ROUTE).methods("POST"_method, "GET"_method)(*this);
+void LineRoute::Register(App& app) {
+	CROW_ROUTE(app, LINE_ROUTE_ROUTE)
+	    .methods("POST"_method, "GET"_method)(*this);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response
-LineRoute::impl(HttpGet, const Request& req, int bid, int pid, int lid) const
-{
+Route::Response LineRoute::impl(HttpGet, const Request& req, int bid, int pid,
+				int lid) const {
 	auto conn = connection();
 	auto session = this->session(req);
 	assert(conn);
@@ -40,22 +38,20 @@ LineRoute::impl(HttpGet, const Request& req, int bid, int pid, int lid) const
 	SessionLock lock(*session);
 
 	auto line = session->find(conn, bid, pid, lid);
-	if (not line)
-		return not_found();
+	if (not line) return not_found();
 	assert(line);
 	Json j;
 	return j << *line;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response
-LineRoute::impl(HttpPost, const Request& req, int bid, int pid, int lid) const
-{
+Route::Response LineRoute::impl(HttpPost, const Request& req, int bid, int pid,
+				int lid) const {
 	CROW_LOG_DEBUG << "(LineRoute) body: " << req.body;
 	auto json = crow::json::load(req.body);
 	if (not json["d"].s().size())
-		THROW(BadRequest, "missing data for book id: ",
-				bid, ", page id: ", pid, ", line id: ", lid);
+		THROW(BadRequest, "missing data for book id: ", bid,
+		      ", page id: ", pid, ", line id: ", lid);
 
 	auto conn = connection();
 	auto session = this->session(req);
@@ -65,16 +61,14 @@ LineRoute::impl(HttpPost, const Request& req, int bid, int pid, int lid) const
 
 	auto line = session->find(conn, bid, pid, lid);
 	if (not line)
-		THROW(NotFound, "Cannot find book id: ", bid,
-				" page id: ", pid, " line id: ", lid);
+		THROW(NotFound, "Cannot find book id: ", bid, " page id: ", pid,
+		      " line id: ", lid);
 	return correct(conn, *line, json);
 }
 
-
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response
-LineRoute::correct(MysqlConnection& conn, Line& line, const crow::json::rvalue& json) const
-{
+Route::Response LineRoute::correct(MysqlConnection& conn, Line& line,
+				   const crow::json::rvalue& json) const {
 	assert(json["d"].s().size());
 	const std::string correction = json["d"].s();
 	WagnerFischer wf;
@@ -98,9 +92,7 @@ LineRoute::correct(MysqlConnection& conn, Line& line, const crow::json::rvalue& 
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void
-LineRoute::log(const WagnerFischer& wf) const
-{
+void LineRoute::log(const WagnerFischer& wf) const {
 	std::string u8;
 	print_with_dotted_circles(wf.gt(), u8);
 	CROW_LOG_INFO << "(LineRoute)    gt: " << u8;
@@ -110,14 +102,14 @@ LineRoute::log(const WagnerFischer& wf) const
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void
-LineRoute::print_with_dotted_circles(const std::wstring& str, std::string& u8)
-{
+void LineRoute::print_with_dotted_circles(const std::wstring& str,
+					  std::string& u8) {
 	static const wchar_t dotted_circle = 0x25cc;
 	u8.clear();
-	for (auto c: str) {
+	for (auto c : str) {
 		if (u_charType(c) == U_NON_SPACING_MARK)
-			utf8::utf32to8(&dotted_circle, &dotted_circle + 1, std::back_inserter(u8));
+			utf8::utf32to8(&dotted_circle, &dotted_circle + 1,
+				       std::back_inserter(u8));
 		utf8::utf32to8(&c, &c + 1, std::back_inserter(u8));
 	}
 }
--- rest/src/api/LineRoute.hpp
+++ rest/src/api/LineRoute.hpp
@@ -4,35 +4,37 @@
 #include "core/CrtpRoute.hpp"
 
 namespace crow {
-	namespace json {
-		class rvalue;
-	}
+namespace json {
+class rvalue;
+}
 }
 
 namespace pcw {
-	class WagnerFischer;
+class WagnerFischer;
 
-	class LineRoute: public CrtpRoute<LineRoute> {
-	public:
-		virtual ~LineRoute() noexcept override = default;
-		virtual void Register(App& app) override;
-		virtual const char* route() const noexcept override {return route_;}
-		virtual const char* name() const noexcept override {return name_;}
+class LineRoute : public CrtpRoute<LineRoute> {
+       public:
+	virtual ~LineRoute() noexcept override = default;
+	virtual void Register(App& app) override;
+	virtual const char* route() const noexcept override { return route_; }
+	virtual const char* name() const noexcept override { return name_; }
 
-		pcw_crtp_route_def_impl__(int,int,int);
-		Response impl(HttpGet, const Request& req, int bid,
-				int pid, int lid) const;
-		Response impl(HttpPost, const Request& req, int bid,
-				int pid, int lid) const;
+	pcw_crtp_route_def_impl__(int, int, int);
+	Response impl(HttpGet, const Request& req, int bid, int pid,
+		      int lid) const;
+	Response impl(HttpPost, const Request& req, int bid, int pid,
+		      int lid) const;
 
-	private:
-		Response correct(MysqlConnection& conn, Line& line, const crow::json::rvalue& data) const;
-		void log(const WagnerFischer& wf) const;
-		static void print_with_dotted_circles(const std::wstring& str, std::string& u8);
+       private:
+	Response correct(MysqlConnection& conn, Line& line,
+			 const crow::json::rvalue& data) const;
+	void log(const WagnerFischer& wf) const;
+	static void print_with_dotted_circles(const std::wstring& str,
+					      std::string& u8);
 
-		static const char* route_;
-		static const char* name_;
-	};
+	static const char* route_;
+	static const char* name_;
+};
 }
 
-#endif // pcw_LineRoute_hpp__
+#endif  // pcw_LineRoute_hpp__
