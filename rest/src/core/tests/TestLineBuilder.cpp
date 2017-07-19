48d17b90f2cd8b99d61a4cf9c4f6b81fb07f0343
flo <flo@cis.lmu.de>
2017-07-19 09:22:16 +0200

    indentation

--- rest/src/core/tests/TestLineBuilder.cpp
+++ rest/src/core/tests/TestLineBuilder.cpp
@@ -3,8 +3,8 @@
 
 #include <boost/test/unit_test.hpp>
 #include <iostream>
-#include "core/LineBuilder.hpp"
 #include "core/Line.hpp"
+#include "core/LineBuilder.hpp"
 #include "core/Page.hpp"
 
 using namespace pcw;
@@ -17,8 +17,7 @@ struct LineBuilderFixture {
 BOOST_FIXTURE_TEST_SUITE(LineBuilder, LineBuilderFixture)
 
 ////////////////////////////////////////////////////////////////////////////////
-BOOST_AUTO_TEST_CASE(ResetTest)
-{
+BOOST_AUTO_TEST_CASE(ResetTest) {
 	auto first = builder.build();
 	builder.reset();
 	auto second = builder.build();
@@ -26,22 +25,19 @@ BOOST_AUTO_TEST_CASE(ResetTest)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-BOOST_AUTO_TEST_CASE(ImgTest)
-{
+BOOST_AUTO_TEST_CASE(ImgTest) {
 	builder.set_image_path("image");
 	BOOST_CHECK_EQUAL(builder.build()->img, "image");
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-BOOST_AUTO_TEST_CASE(BoxTest)
-{
+BOOST_AUTO_TEST_CASE(BoxTest) {
 	builder.set_box(Box{1, 2, 3, 4});
 	BOOST_CHECK_EQUAL(builder.build()->box, Box(1, 2, 3, 4));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-BOOST_AUTO_TEST_CASE(AppendWcharsTest)
-{
+BOOST_AUTO_TEST_CASE(AppendWcharsTest) {
 	builder.append(L'ф', 1, 1.0);
 	builder.append(L'о', 2, 1.0);
 	builder.append(L'н', 3, 1.0);
@@ -51,15 +47,13 @@ BOOST_AUTO_TEST_CASE(AppendWcharsTest)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-BOOST_AUTO_TEST_CASE(AppendWstrTest)
-{
+BOOST_AUTO_TEST_CASE(AppendWstrTest) {
 	builder.append(L"фонды", 5, 1.0);
 	BOOST_CHECK_EQUAL(builder.build()->ocr(), "фонды");
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-BOOST_AUTO_TEST_CASE(AppendU8strTest)
-{
+BOOST_AUTO_TEST_CASE(AppendU8strTest) {
 	builder.append("фонды", 5, 1.0);
 	BOOST_CHECK_EQUAL(builder.build()->ocr(), "фонды");
 }
