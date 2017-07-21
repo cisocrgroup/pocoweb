e86c9e2cbb72f3c3a72e36c52cc58cf6bca20459
flo <flo@cis.lmu.de>
2017-07-21 09:34:18 +0200

    indentation

--- plugins/profiler/ProfilerRoute.cpp
+++ plugins/profiler/ProfilerRoute.cpp
@@ -1,13 +1,13 @@
-#include <crow/app.h>
-#include "core/App.hpp"
-#include "core/Book.hpp"
-#include "utils/Maybe.hpp"
 #include "core/Profile.hpp"
-#include "core/Session.hpp"
+#include <crow/app.h>
 #include "Config.hpp"
 #include "LocalProfiler.hpp"
-#include "RemoteProfiler.hpp"
 #include "ProfilerRoute.hpp"
+#include "RemoteProfiler.hpp"
+#include "core/App.hpp"
+#include "core/Book.hpp"
+#include "core/Session.hpp"
+#include "utils/Maybe.hpp"
 
 #define PROFILER_ROUTE_ROUTE "/books/<int>/profile"
 
@@ -19,22 +19,17 @@ const char* ProfilerRoute::name_ = "ProfilerRoute";
 
 ////////////////////////////////////////////////////////////////////////////////
 ProfilerRoute::ProfilerRoute()
-	: mutex_(std::make_unique<Mutex>())
-	, jobs_(std::make_shared<Jobs>())
-{
+    : mutex_(std::make_unique<Mutex>()), jobs_(std::make_shared<Jobs>()) {}
+
+////////////////////////////////////////////////////////////////////////////////
+void ProfilerRoute::Register(App& app) {
+	CROW_ROUTE(app, PROFILER_ROUTE_ROUTE)
+	    .methods("GET"_method, "POST"_method)(*this);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void
-ProfilerRoute::Register(App& app)
-{
-	CROW_ROUTE(app, PROFILER_ROUTE_ROUTE).methods("GET"_method, "POST"_method)(*this);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-ProfilerRoute::Response
-ProfilerRoute::impl(HttpGet, const Request& req, int bid) const
-{
+ProfilerRoute::Response ProfilerRoute::impl(HttpGet, const Request& req,
+					    int bid) const {
 	// query book
 	auto book = get_book(req, bid);
 	assert(book);
@@ -46,52 +41,51 @@ ProfilerRoute::impl(HttpGet, const Request& req, int bid) const
 		auto future = jobs_->find(bid);
 		assert(future != end(*jobs_));
 		auto status = future->second.wait_for(amount);
-		if (status == std::future_status::ready) { // job is done
-			CROW_LOG_INFO << "(ProfilerRoute) Job id: " << book->id()
-				      << " done";
-			auto res = future->second.get(); // should not block
-			jobs_->erase(future); // remove job
+		if (status == std::future_status::ready) {  // job is done
+			CROW_LOG_INFO
+			    << "(ProfilerRoute) Job id: " << book->id()
+			    << " done";
+			auto res = future->second.get();  // should not block
+			jobs_->erase(future);		  // remove job
 			return handle_new_profile(req, res);
-		} else { // job is not done yet
-			CROW_LOG_INFO << "(ProfilerRoute) Job id: " << book->id()
-				      << " not done yet";
+		} else {  // job is not done yet
+			CROW_LOG_INFO
+			    << "(ProfilerRoute) Job id: " << book->id()
+			    << " not done yet";
 			return accepted();
 		}
-	} else { // no such job
+	} else {  // no such job
 		return not_implemented();
 	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-ProfilerRoute::Response
-ProfilerRoute::handle_new_profile(const Request& req, const Result& res) const
-{
+ProfilerRoute::Response ProfilerRoute::handle_new_profile(
+    const Request& req, const Result& res) const {
 	auto profile = std::move(res.get());
 	return not_implemented();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-ProfilerRoute::Response
-ProfilerRoute::impl(HttpPost, const Request& req, int bid) const
-{
+ProfilerRoute::Response ProfilerRoute::impl(HttpPost, const Request& req,
+					    int bid) const {
 	// query book
 	auto book = get_book(req, bid);
 	assert(book);
 
 	Lock lock(*mutex_);
 	if (not is_running_job_id(book->id())) {
-		jobs_->emplace(book->id(), std::async(std::launch::async, [book]() {
-			auto profiler = get_profiler(book);
-			return profiler->profile();
-		}));
+		jobs_->emplace(book->id(),
+			       std::async(std::launch::async, [book]() {
+				       auto profiler = get_profiler(book);
+				       return profiler->profile();
+			       }));
 	}
 	return accepted();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-ProfilerUptr
-ProfilerRoute::get_profiler(ConstBookSptr book)
-{
+ProfilerUptr ProfilerRoute::get_profiler(ConstBookSptr book) {
 	if (Config::get().local())
 		return std::make_unique<LocalProfiler>(book);
 	else
@@ -99,9 +93,7 @@ ProfilerRoute::get_profiler(ConstBookSptr book)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-ConstBookSptr
-ProfilerRoute::get_book(const Request& req, int bid) const
-{
+ConstBookSptr ProfilerRoute::get_book(const Request& req, int bid) const {
 	auto conn = connection();
 	auto session = this->session(req);
 	assert(conn);
@@ -109,10 +101,9 @@ ProfilerRoute::get_book(const Request& req, int bid) const
 	pcw::SessionLock sessionlock(*session);
 	auto view = session->find(conn, bid);
 
-	if (not view)
-		THROW(pcw::NotFound, "Cannot find book id: ", bid);
+	if (not view) THROW(pcw::NotFound, "Cannot find book id: ", bid);
 	auto book = std::dynamic_pointer_cast<const pcw::Book>(
-			view->origin().shared_from_this());
+	    view->origin().shared_from_this());
 	if (not book)
 		THROW(pcw::NotFound, "Cannot find origin of book id: ", bid);
 	return book;
--- plugins/profiler/ProfilerRoute.hpp
+++ plugins/profiler/ProfilerRoute.hpp
@@ -1,54 +1,57 @@
 #ifndef profiler_ProfilerRoute_hpp__
 #define profiler_ProfilerRoute_hpp__
 
+#include <crow/logging.h>
 #include <memory>
 #include <mutex>
-#include <crow/logging.h>
 #include "core/CrtpRoute.hpp"
 
 namespace pcw {
-	class Book;
-	using ConstBookSptr = std::shared_ptr<const Book>;
-	class Profile;
-	template<class T> class Maybe;
+class Book;
+using ConstBookSptr = std::shared_ptr<const Book>;
+class Profile;
+template <class T>
+class Maybe;
 }
 
 namespace profiler {
-	using ConstBookSptr = pcw::ConstBookSptr;
-	class Profiler;
-	using ProfilerUptr = std::unique_ptr<Profiler>;
+using ConstBookSptr = pcw::ConstBookSptr;
+class Profiler;
+using ProfilerUptr = std::unique_ptr<Profiler>;
 
-	class ProfilerRoute: public pcw::CrtpRoute<ProfilerRoute> {
-	public:
-		ProfilerRoute();
+class ProfilerRoute : public pcw::CrtpRoute<ProfilerRoute> {
+       public:
+	ProfilerRoute();
 
-		virtual void Register(App& app) override;
-		virtual const char* route() const noexcept override {return route_;}
-		virtual const char* name() const noexcept override {return name_;}
+	virtual void Register(App& app) override;
+	virtual const char* route() const noexcept override { return route_; }
+	virtual const char* name() const noexcept override { return name_; }
 
-		pcw_crtp_route_def_impl__(int);
-		Response impl(HttpGet, const Request& req, int bid) const;
-		Response impl(HttpPost, const Request& req, int bid) const;
+	pcw_crtp_route_def_impl__(int);
+	Response impl(HttpGet, const Request& req, int bid) const;
+	Response impl(HttpPost, const Request& req, int bid) const;
 
-	private:
-		using Mutex = std::mutex;
-		using MutexPtr = std::shared_ptr<Mutex>;
-		using Lock = std::lock_guard<Mutex>;
-		using Jobs = std::unordered_map<int, std::future<pcw::Maybe<pcw::Profile>>>;
-		using JobsPtr = std::shared_ptr<Jobs>;
-		using Result = pcw::Maybe<pcw::Profile>;
+       private:
+	using Mutex = std::mutex;
+	using MutexPtr = std::shared_ptr<Mutex>;
+	using Lock = std::lock_guard<Mutex>;
+	using Jobs =
+	    std::unordered_map<int, std::future<pcw::Maybe<pcw::Profile>>>;
+	using JobsPtr = std::shared_ptr<Jobs>;
+	using Result = pcw::Maybe<pcw::Profile>;
 
-		bool is_running_job_id(int id) const noexcept {
-			return jobs_->count(id);
-		}
-		ConstBookSptr get_book(const Request& req, int bid) const;
-		static ProfilerUptr get_profiler(ConstBookSptr book);
-		Response handle_new_profile(const Request& req, const Result& res) const;
+	bool is_running_job_id(int id) const noexcept {
+		return jobs_->count(id);
+	}
+	ConstBookSptr get_book(const Request& req, int bid) const;
+	static ProfilerUptr get_profiler(ConstBookSptr book);
+	Response handle_new_profile(const Request& req,
+				    const Result& res) const;
 
-		static const char *route_, *name_;
-		MutexPtr mutex_;
-		JobsPtr jobs_;
-	};
+	static const char *route_, *name_;
+	MutexPtr mutex_;
+	JobsPtr jobs_;
+};
 }
 
-#endif // profiler_ProfilerRoute_hpp__
+#endif  // profiler_ProfilerRoute_hpp__
