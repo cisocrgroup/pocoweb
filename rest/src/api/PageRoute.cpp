a25854580932b19d58ede31bdf3b80d2dca17e30
flo <flo@cis.lmu.de>
2017-07-25 09:40:06 +0200

    indentation

--- rest/src/api/PageRoute.cpp
+++ rest/src/api/PageRoute.cpp
@@ -1,16 +1,16 @@
-#include <regex>
+#include "core/Page.hpp"
 #include <crow.h>
+#include <regex>
+#include "PageRoute.hpp"
+#include "core/Book.hpp"
+#include "core/BookDirectoryBuilder.hpp"
+#include "core/Package.hpp"
+#include "core/Session.hpp"
+#include "core/User.hpp"
+#include "core/WagnerFischer.hpp"
+#include "core/jsonify.hpp"
 #include "utils/Error.hpp"
 #include "utils/ScopeGuard.hpp"
-#include "core/jsonify.hpp"
-#include "core/User.hpp"
-#include "core/Page.hpp"
-#include "core/Book.hpp"
-#include "PageRoute.hpp"
-#include "core/Session.hpp"
-#include "core/Package.hpp"
-#include "core/BookDirectoryBuilder.hpp"
-#include "core/WagnerFischer.hpp"
 
 #define PAGE_ROUTE_ROUTE_1 "/books/<int>/pages/<int>"
 #define PAGE_ROUTE_ROUTE_2 "/books/<int>/pages/<int>/lines"
@@ -22,19 +22,13 @@
 using namespace pcw;
 
 ////////////////////////////////////////////////////////////////////////////////
-const char* PageRoute::route_ =
-	PAGE_ROUTE_ROUTE_1 ","
-	PAGE_ROUTE_ROUTE_2 ","
-	PAGE_ROUTE_ROUTE_3 ","
-	PAGE_ROUTE_ROUTE_4 ","
-	PAGE_ROUTE_ROUTE_5 ","
-	PAGE_ROUTE_ROUTE_6;
+const char* PageRoute::route_ = PAGE_ROUTE_ROUTE_1
+    "," PAGE_ROUTE_ROUTE_2 "," PAGE_ROUTE_ROUTE_3 "," PAGE_ROUTE_ROUTE_4
+    "," PAGE_ROUTE_ROUTE_5 "," PAGE_ROUTE_ROUTE_6;
 const char* PageRoute::name_ = "PageRoute";
 
 ////////////////////////////////////////////////////////////////////////////////
-void
-PageRoute::Register(App& app)
-{
+void PageRoute::Register(App& app) {
 	CROW_ROUTE(app, PAGE_ROUTE_ROUTE_1).methods("GET"_method)(*this);
 	CROW_ROUTE(app, PAGE_ROUTE_ROUTE_2).methods("GET"_method)(*this);
 	CROW_ROUTE(app, PAGE_ROUTE_ROUTE_3).methods("GET"_method)(*this);
@@ -44,19 +38,18 @@ PageRoute::Register(App& app)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response
-PageRoute::impl(HttpGet, const Request& req, int bid, int pid) const
-{
+Route::Response PageRoute::impl(HttpGet, const Request& req, int bid,
+				int pid) const {
 	auto conn = connection();
 	auto session = this->session(req);
 	assert(conn);
 	assert(session);
 	SessionLock lock(*session);
-	CROW_LOG_DEBUG << "(PageRoute) searching for book id: " << bid << " page id: " << pid;
+	CROW_LOG_DEBUG << "(PageRoute) searching for book id: " << bid
+		       << " page id: " << pid;
 	auto project = session->find(conn, bid);
 	auto page = session->find(conn, bid, pid);
-	if (not page or not project)
-		return not_found();
+	if (not page or not project) return not_found();
 	assert(page);
 	assert(project);
 	Json j;
@@ -64,9 +57,7 @@ PageRoute::impl(HttpGet, const Request& req, int bid, int pid) const
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response
-PageRoute::impl(HttpGet, const Request& req, int bid) const
-{
+Route::Response PageRoute::impl(HttpGet, const Request& req, int bid) const {
 	auto conn = connection();
 	auto session = this->session(req);
 	assert(conn);
@@ -76,19 +67,17 @@ PageRoute::impl(HttpGet, const Request& req, int bid) const
 	auto book = session->find(conn, bid);
 	CROW_LOG_DEBUG << "(PageRoute) found book id: " << book->id();
 	bool first = false;
-	if (not book)
-		return not_found();
+	if (not book) return not_found();
 	if (strcasestr(req.url.data(), "/first"))
 		first = true;
 	else if (strcasestr(req.url.data(), "/last"))
 		first = false;
 	else
 		return bad_request();
-	if (book->empty())
-		return not_found();
+	if (book->empty()) return not_found();
 	CROW_LOG_DEBUG << "(PageRoute) book id: " << book->id()
 		       << " size: " << book->size();
-	for (const auto p: *book) {
+	for (const auto p : *book) {
 		CROW_LOG_DEBUG << "(PageRoute) page id: " << p->id();
 	}
 
@@ -107,9 +96,8 @@ PageRoute::impl(HttpGet, const Request& req, int bid) const
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response
-PageRoute::impl(HttpGet, const Request& req, int bid, int pid, int val) const
-{
+Route::Response PageRoute::impl(HttpGet, const Request& req, int bid, int pid,
+				int val) const {
 	auto conn = connection();
 	auto session = this->session(req);
 	assert(conn);
@@ -117,8 +105,7 @@ PageRoute::impl(HttpGet, const Request& req, int bid, int pid, int val) const
 	SessionLock lock(*session);
 
 	auto book = session->find(conn, bid);
-	if (not book)
-		return not_found();
+	if (not book) return not_found();
 	if (strcasestr(req.url.data(), "/next/"))
 		return next(*book, pid, std::abs(val));
 	else if (strcasestr(req.url.data(), "/prev/"))
@@ -128,31 +115,24 @@ PageRoute::impl(HttpGet, const Request& req, int bid, int pid, int val) const
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response
-PageRoute::next(const Project& book, int pid, int val) const
-{
+Route::Response PageRoute::next(const Project& book, int pid, int val) const {
 	auto page = book.next(pid, val);
-	if (not page)
-		return not_found();
+	if (not page) return not_found();
 	Json j;
 	return print(j, *page, book);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response
-PageRoute::prev(const Project& book, int pid, int val) const
-{
+Route::Response PageRoute::prev(const Project& book, int pid, int val) const {
 	auto page = book.next(pid, -val);
-	if (not page)
-		return not_found();
+	if (not page) return not_found();
 	Json j;
 	return print(j, *page, book);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response
-PageRoute::print(Json& json, const Page& page, const Project& project)
-{
+Route::Response PageRoute::print(Json& json, const Page& page,
+				 const Project& project) {
 	json << page;
 	const auto nextpage = project.next(page.id(), 1);
 	const auto prevpage = project.next(page.id(), -1);
@@ -164,4 +144,3 @@ PageRoute::print(Json& json, const Page& page, const Project& project)
 	json["bookId"] = project.origin().id();
 	return json;
 }
-
