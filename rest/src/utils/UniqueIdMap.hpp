f395c50559db19b9776b1c40591fadce390b6f0c
flo <flo@cis.lmu.de>
2017-08-07 16:13:59 +0200

    unique id map returns a pair consiting of the id and a flag that specifies if the id was newly created

--- rest/src/utils/UniqueIdMap.hpp
+++ rest/src/utils/UniqueIdMap.hpp
@@ -10,13 +10,15 @@ class UniqueIdMap : private std::map<T, int> {
 	using base = std::map<T, int>;
 
        public:
-	int operator[](const T& t) {
+	std::pair<int, bool> operator[](const T& t) {
 		auto i = base::find(t);
+		auto isnew = false;
 		if (i == base::end()) {
 			auto newid = static_cast<int>(base::size()) + 1;
 			i = base::emplace_hint(i, t, newid);
+			isnew = true;
 		}
-		return i->second;
+		return {i->second, isnew};
 	}
 };
 }
--- rest/src/utils/tests/TestUniqueIdMap.cpp
+++ rest/src/utils/tests/TestUniqueIdMap.cpp
@@ -10,10 +10,21 @@ using namespace pcw;
 ////////////////////////////////////////////////////////////////////////////////
 BOOST_AUTO_TEST_CASE(UniqueIds) {
 	UniqueIdMap<std::string> ids;
-	BOOST_CHECK_EQUAL(ids["first"], 1);
-	BOOST_CHECK_EQUAL(ids["second"], 2);
-	BOOST_CHECK_EQUAL(ids["third"], 3);
-	BOOST_CHECK_EQUAL(ids["first"], 1);
-	BOOST_CHECK_EQUAL(ids["second"], 2);
-	BOOST_CHECK_EQUAL(ids["third"], 3);
+	BOOST_CHECK_EQUAL(ids["first"].first, 1);
+	BOOST_CHECK_EQUAL(ids["second"].first, 2);
+	BOOST_CHECK_EQUAL(ids["third"].first, 3);
+	BOOST_CHECK_EQUAL(ids["first"].first, 1);
+	BOOST_CHECK_EQUAL(ids["second"].first, 2);
+	BOOST_CHECK_EQUAL(ids["third"].first, 3);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+BOOST_AUTO_TEST_CASE(NewIds) {
+	UniqueIdMap<std::string> ids;
+	BOOST_CHECK_EQUAL(ids["first"].second, true);
+	BOOST_CHECK_EQUAL(ids["second"].second, true);
+	BOOST_CHECK_EQUAL(ids["third"].second, true);
+	BOOST_CHECK_EQUAL(ids["first"].second, false);
+	BOOST_CHECK_EQUAL(ids["second"].second, false);
+	BOOST_CHECK_EQUAL(ids["third"].second, false);
 }
