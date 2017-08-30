050104e96b7a9357654b2e77e3200d456442c752
flo <flo@cis.lmu.de>
2017-08-30 13:15:24 +0200

    add missing `,`

--- frontend/resources/config.php
+++ frontend/resources/config.php
@@ -94,7 +94,7 @@ $config = array(
 		500 => "Internal Server Error",
 		501 => "Not Implemented",
 		502 => "Bad Gateway",
-		503 => "Service Unavailable"
+		503 => "Service Unavailable",
 		504 => "Gateway Timeout",
 		505 => "HTTP Version Not Supported",
 	),
