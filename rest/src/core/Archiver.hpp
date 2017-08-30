6e677578bab6300a239904730db564f7a726c36b
flo <flo@cis.lmu.de>
2017-08-30 14:08:07 +0200

    downloads contain the adaptive token set

--- rest/src/core/Archiver.hpp
+++ rest/src/core/Archiver.hpp
@@ -3,6 +3,7 @@
 
 #include <boost/filesystem/path.hpp>
 #include <memory>
+#include "database/mysql.hpp"
 
 namespace pcw {
 class Project;
@@ -11,12 +12,13 @@ class Line;
 class Archiver {
        public:
 	using Path = boost::filesystem::path;
-	Archiver(const Project& p);
+	Archiver(const Project& p, MysqlConnection& conn);
 	Path operator()() const;
 
        private:
 	void zip(const Path& dir, const Path& archive) const;
 	void copy_files(const Path& dir) const;
+	void write_adaptive_token_set(const Path& dir) const;
 	void write_gt_file(const Line& line, const Path& to) const;
 	Path archive_name() const noexcept;
 	static Path get_tmp_file(const Path& source, const Path& tmpdir);
@@ -24,6 +26,7 @@ class Archiver {
 	static Path remove_common_base_path(const Path& p, const Path& base);
 
 	const std::shared_ptr<const Project> project_;
+	MysqlConnection& conn_;
 };
 }
 #endif  // pcw_Archiver_hpp__
