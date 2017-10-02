d83be56b2f63eed04af1c73a0c5a4c15cf4da54d
flo <flo@cis.lmu.de>
2017-10-02 11:18:47 +0200

    make cache LRU

--- rest/src/core/Cache.hpp
+++ rest/src/core/Cache.hpp
@@ -37,7 +37,6 @@ class Cache : private std::list<std::shared_ptr<T>> {
 	using Mutex = std::mutex;
 	using Lock = std::lock_guard<Mutex>;
 
-	void put_impl(value_type t);
 	template <class F, class G>
 	value_type get_impl(F f, G g);
 
@@ -50,7 +49,12 @@ class Cache : private std::list<std::shared_ptr<T>> {
 template <class T>
 void pcw::Cache<T>::put(value_type t) {
 	Lock lock(mutex_);
-	put_impl(std::move(t));
+	if (t) {
+		Base::push_front(t);
+		while (Base::size() > n_) {
+			Base::pop_back();
+		}
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -63,21 +67,6 @@ void pcw::Cache<T>::del(int id) {
 		return ptr and ptr->id() == id;
 	});
 	if (i != end(*this)) this->erase(i);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-template <class T>
-void pcw::Cache<T>::put_impl(value_type t) {
-	assert(not mutex_.try_lock());
-	// never put nullptr
-	if (t) {
-		// new elements are inserted at the back of the cache
-		if (Base::size() < n_) {
-			Base::push_back(std::move(t));
-		} else {
-			Base::back() = std::move(t);
-		}
-	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -109,12 +98,16 @@ typename pcw::Cache<T>::value_type pcw::Cache<T>::get_impl(F f, G g) {
 	auto i = std::find_if(begin(), end(), f);
 	if (i == end()) {
 		auto t = g();
-		put_impl(t);
+		Base::push_front(t);
+		while (Base::size() > n_) {
+			Base::pop_back();
+		}
 		return t;
 	} else if (i != begin()) {
-		auto j = std::prev(i);
-		std::swap(*i, *j);
-		return *j;
+		auto t = *i;
+		auto b = begin();
+		std::swap(*b, *i);
+		return t;
 	} else {
 		return *i;
 	}
