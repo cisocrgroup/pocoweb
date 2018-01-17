01d83c5046bf4c4d42005d62b3280bbceceeac39
flo <flo@cis.lmu.de>
2018-01-17 18:31:47 +0100

    fix generation of profiler suggestions

--- rest/src/profiler/Profiler.hpp
+++ rest/src/profiler/Profiler.hpp
@@ -9,25 +9,24 @@ namespace pcw {
 class Book;
 using ConstBookSptr = std::shared_ptr<const Book>;
 class Profile;
-template <class T>
-class Maybe;
+template <class T> class Maybe;
 using ConstBookSptr = ConstBookSptr;
 
 class Profiler {
-       public:
-	Profiler(ConstBookSptr book) : book_(std::move(book)) {}
-	virtual ~Profiler() noexcept = default;
-	pcw::Maybe<Profile> profile() noexcept;
-	pcw::Maybe<std::vector<std::string>> languages() noexcept;
-	const Book& book() const noexcept { return *book_; }
+public:
+  Profiler(ConstBookSptr book) : book_(std::move(book)) {}
+  virtual ~Profiler() noexcept = default;
+  pcw::Maybe<Profile> profile() noexcept;
+  pcw::Maybe<std::vector<std::string>> languages() noexcept;
+  const Book &book() const noexcept { return *book_; }
 
-       protected:
-	virtual Profile do_profile() = 0;
-	virtual std::vector<std::string> do_languages() = 0;
+protected:
+  virtual Profile do_profile() = 0;
+  virtual std::vector<std::string> do_languages() = 0;
 
-       private:
-	ConstBookSptr book_;
+private:
+  ConstBookSptr book_;
 };
 }
 
-#endif  // pcw_Profiler_hpp__
+#endif // pcw_Profiler_hpp__
