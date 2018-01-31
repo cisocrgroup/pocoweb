7491828598b653154747bac39d06cee692176f85
flo <flo@cis.lmu.de>
2018-01-31 22:53:35 +0100

    Change logging level to debug

--- rest/src/core/App.cpp
+++ rest/src/core/App.cpp
@@ -8,34 +8,45 @@
 using namespace pcw;
 
 ////////////////////////////////////////////////////////////////////////////////
-App::App(const char *config)
-    : routes_(), plugins_(), app_(),
-      cache_(std::make_shared<AppCache>(100, 100, 100)),
-      config_(std::make_shared<Config>(Config::load(config))),
-      session_store_(std::make_shared<SessionStore>()),
-      connection_pool_(std::make_shared<MysqlConnectionPool>(
-          config_->db.connections, mysqlConnectionConfigFromConfig(*config_))) {
+App::App(const char* config)
+  : routes_()
+  , plugins_()
+  , app_()
+  , cache_(std::make_shared<AppCache>(100, 100, 100))
+  , config_(std::make_shared<Config>(Config::load(config)))
+  , session_store_(std::make_shared<SessionStore>())
+  , connection_pool_(std::make_shared<MysqlConnectionPool>(
+      config_->db.connections,
+      mysqlConnectionConfigFromConfig(*config_)))
+{
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-App::~App() noexcept { stop(); }
+App::~App() noexcept
+{
+  stop();
+}
 
 ////////////////////////////////////////////////////////////////////////////////
-void App::run() {
+void
+App::run()
+{
   assert(config_);
   if (not app_)
     app_ = std::make_unique<Route::App>();
-  CROW_LOG_INFO << "(App) Starting server version " << App::version_str()
-                << " on " << config_->daemon.host << ":"
-                << config_->daemon.port;
+  CROW_LOG_DEBUG << "(App) Starting server version " << App::version_str()
+                 << " on " << config_->daemon.host << ":"
+                 << config_->daemon.port;
   app_->port(config_->daemon.port)
-      .concurrency(config_->daemon.threads)
-      .bindaddr(config_->daemon.host)
-      .run();
+    .concurrency(config_->daemon.threads)
+    .bindaddr(config_->daemon.host)
+    .run();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void App::stop() noexcept {
+void
+App::stop() noexcept
+{
   try {
     // order matters here: first delete the server,
     // then delete all routes and then close the plugins
@@ -44,7 +55,7 @@ void App::stop() noexcept {
     }
     routes_.clear();
     plugins_.clear();
-  } catch (const std::exception &e) {
+  } catch (const std::exception& e) {
     CROW_LOG_ERROR << "(App) Error: " << e.what();
   } catch (...) {
     CROW_LOG_ERROR << "(App) Unknown error";
@@ -52,7 +63,9 @@ void App::stop() noexcept {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void App::Register(RoutePtr route) {
+void
+App::Register(RoutePtr route)
+{
   if (route) {
     if (not app_)
       app_ = std::make_unique<Route::App>();
@@ -68,16 +81,18 @@ void App::Register(RoutePtr route) {
       route->Register(*app_);
       routes_.push_back(std::move(route));
       log(*routes_.back());
-    } catch (const std::exception &e) {
+    } catch (const std::exception& e) {
       CROW_LOG_ERROR << "(App) Could not register: " << e.what();
     }
   }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void App::log(const Route &route) const {
+void
+App::log(const Route& route) const
+{
   for (auto i = route.route(); i;) {
-    const char *e = strchr(i, ',');
+    const char* e = strchr(i, ',');
     std::string r;
     if (e) {
       r.assign(i, e);
@@ -86,19 +101,21 @@ void App::log(const Route &route) const {
       r.assign(i);
       i = e;
     }
-    CROW_LOG_INFO << "(App) Registered route " << route.name() << ": " << r
-                  << " [" << &route << "]";
+    CROW_LOG_DEBUG << "(App) Registered route " << route.name() << ": " << r
+                   << " [" << &route << "]";
   }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void App::register_plugins() {
+void
+App::register_plugins()
+{
   assert(config_);
   if (not app_)
     app_ = std::make_unique<Route::App>();
-  for (const auto &p : config_->plugins.configs) {
+  for (const auto& p : config_->plugins.configs) {
     auto path = p.second.get<std::string>("path");
-    CROW_LOG_INFO << "(App) Registering plugin " << p.first << ": " << path;
+    CROW_LOG_DEBUG << "(App) Registering plugin " << p.first << ": " << path;
     pcw::Plugin plugin(path);
     plugin(p.first, *this);
     plugins_.push_back(std::move(plugin));
@@ -106,22 +123,38 @@ void App::register_plugins() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-int App::version() noexcept {
+int
+App::version() noexcept
+{
   return (100 * 100 * PCW_API_VERSION_MAJOR) + (100 * PCW_API_VERSION_MINOR) +
          (1 * PCW_API_VERSION_PATCH);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-int App::version_major() noexcept { return PCW_API_VERSION_MAJOR; }
+int
+App::version_major() noexcept
+{
+  return PCW_API_VERSION_MAJOR;
+}
 
 ////////////////////////////////////////////////////////////////////////////////
-int App::version_minor() noexcept { return PCW_API_VERSION_MINOR; }
+int
+App::version_minor() noexcept
+{
+  return PCW_API_VERSION_MINOR;
+}
 
 ////////////////////////////////////////////////////////////////////////////////
-int App::version_patch() noexcept { return PCW_API_VERSION_PATCH; }
+int
+App::version_patch() noexcept
+{
+  return PCW_API_VERSION_PATCH;
+}
 
 ////////////////////////////////////////////////////////////////////////////////
-const char *App::version_str() noexcept {
+const char*
+App::version_str() noexcept
+{
 #define QQUOTE(x) #x
 #define QUOTE(x) QQUOTE(x)
 
