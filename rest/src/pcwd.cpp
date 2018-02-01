aa6e46876f49bf853472109e2796ecf8674a4399
flo <flo@cis.lmu.de>
2018-02-01 11:55:03 +0100

    Indentation

--- rest/src/pcwd.cpp
+++ rest/src/pcwd.cpp
@@ -30,34 +30,48 @@
 
 using namespace pcw;
 using AppPtr = std::unique_ptr<App>;
-static int run(int argc, char **argv);
-static void run(App &app);
-static AppPtr get_app(int argc, char **argv);
-static void change_user_and_group(const Config &config);
-static void detach(const Config &config);
-static void create_base_directory(const Config &config);
-static const char *find_config_file(int argc, char **argv);
-static void write_pidfile(const Config &config);
+static int
+run(int argc, char** argv);
+static void
+run(App& app);
+static AppPtr
+get_app(int argc, char** argv);
+static void
+change_user_and_group(const Config& config);
+static void
+detach(const Config& config);
+static void
+create_base_directory(const Config& config);
+static const char*
+find_config_file(int argc, char** argv);
+static void
+write_pidfile(const Config& config);
 
 ////////////////////////////////////////////////////////////////////////////////
-int main(int argc, char **argv) {
+int
+main(int argc, char** argv)
+{
   try {
     return run(argc, argv);
-  } catch (const std::exception &e) {
+  } catch (const std::exception& e) {
     CROW_LOG_ERROR << e.what();
   }
   return 1;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-int run(int argc, char **argv) {
+int
+run(int argc, char** argv)
+{
   auto app = get_app(argc, argv);
   run(*app);
   return 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void run(App &app) {
+void
+run(App& app)
+{
   change_user_and_group(app.config());
   create_base_directory(app.config());
   detach(app.config());
@@ -82,7 +96,9 @@ void run(App &app) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void change_user_and_group(const Config &config) {
+void
+change_user_and_group(const Config& config)
+{
   const auto user = config.daemon.user.data();
   errno = 0;
   const auto pw = getpwnam(user);
@@ -99,7 +115,9 @@ void change_user_and_group(const Config &config) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void detach(const Config &config) {
+void
+detach(const Config& config)
+{
   if (config.daemon.detach) {
     write_pidfile(config);
     if (daemon(0, 0) != 0) {
@@ -109,7 +127,9 @@ void detach(const Config &config) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-AppPtr get_app(int argc, char **argv) {
+AppPtr
+get_app(int argc, char** argv)
+{
   auto app = std::make_unique<pcw::App>(find_config_file(argc, argv));
   app->config().setup_logging();
   app->config().LOG();
@@ -117,17 +137,21 @@ AppPtr get_app(int argc, char **argv) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void create_base_directory(const Config &config) {
+void
+create_base_directory(const Config& config)
+{
   boost::system::error_code ec;
   const auto dir = config.daemon.basedir + "/" + config.daemon.projectdir;
   if (not boost::filesystem::create_directories(dir, ec) and ec.value() != 0) {
-    throw std::system_error(ec.value(), std::system_category(),
-                            config.daemon.basedir);
+    throw std::system_error(
+      ec.value(), std::system_category(), config.daemon.basedir);
   }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-const char *find_config_file(int argc, char **argv) {
+const char*
+find_config_file(int argc, char** argv)
+{
   if (not(argc > 1)) {
     throw std::runtime_error("usage: " + std::string(argv[0]) + " config");
   }
@@ -135,11 +159,13 @@ const char *find_config_file(int argc, char **argv) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void write_pidfile(const Config &config) {
+void
+write_pidfile(const Config& config)
+{
   std::ofstream out(config.daemon.pidfile);
   if (not out.good()) {
-    throw std::system_error(errno, std::system_category(),
-                            config.daemon.pidfile);
+    throw std::system_error(
+      errno, std::system_category(), config.daemon.pidfile);
   }
   out << getpid() << std::endl;
   out.close();
