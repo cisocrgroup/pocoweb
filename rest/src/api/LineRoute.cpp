69c464a0833ece3121f897fd072f70c2447416ab
flo <flo@cis.lmu.de>
2018-02-06 10:10:08 +0100

    indentation

--- rest/src/api/LineRoute.cpp
+++ rest/src/api/LineRoute.cpp
@@ -1,8 +1,4 @@
 #include "LineRoute.hpp"
-#include <crow.h>
-#include <unicode/uchar.h>
-#include <utf8.h>
-#include <regex>
 #include "core/Book.hpp"
 #include "core/BookDirectoryBuilder.hpp"
 #include "core/Package.hpp"
@@ -14,6 +10,10 @@
 #include "core/jsonify.hpp"
 #include "utils/Error.hpp"
 #include "utils/ScopeGuard.hpp"
+#include <crow.h>
+#include <regex>
+#include <unicode/uchar.h>
+#include <utf8.h>
 
 #define LINE_ROUTE_ROUTE "/books/<int>/pages/<int>/lines/<int>"
 #define WORD_ROUTE_ROUTE "/books/<int>/pages/<int>/lines/<int>/tokens/<int>"
@@ -21,114 +21,127 @@
 using namespace pcw;
 
 ////////////////////////////////////////////////////////////////////////////////
-const char* LineRoute::route_ = LINE_ROUTE_ROUTE "," WORD_ROUTE_ROUTE;
-const char* LineRoute::name_ = "LineRoute";
+const char *LineRoute::route_ = LINE_ROUTE_ROUTE "," WORD_ROUTE_ROUTE;
+const char *LineRoute::name_ = "LineRoute";
 
 ////////////////////////////////////////////////////////////////////////////////
-void LineRoute::Register(App& app) {
-	CROW_ROUTE(app, LINE_ROUTE_ROUTE)
-	    .methods("GET"_method, "POST"_method)(*this);
-	CROW_ROUTE(app, WORD_ROUTE_ROUTE).methods("POST"_method)(*this);
+void LineRoute::Register(App &app) {
+  CROW_ROUTE(app, LINE_ROUTE_ROUTE).methods("GET"_method, "POST"_method)(*this);
+  CROW_ROUTE(app, WORD_ROUTE_ROUTE).methods("GET"_method, "POST"_method)(*this);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response LineRoute::impl(HttpGet, const Request& req, int bid, int pid,
-				int lid) const {
-	LockedSession session(must_find_session(req));
-	auto conn = must_get_connection();
-	auto line = session->must_find(conn, bid, pid, lid);
-	Json j;
-	return j << *line;
+Route::Response LineRoute::impl(HttpGet, const Request &req, int bid, int pid,
+                                int lid) const {
+  LockedSession session(must_find_session(req));
+  auto conn = must_get_connection();
+  auto line = session->must_find(conn, bid, pid, lid);
+  Json j;
+  return j << *line;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response LineRoute::impl(HttpPost, const Request& req, int pid, int p,
-				int lid) const {
-	const auto json = crow::json::load(req.body);
-	const auto c = get<std::string>(json, "correction");
-	if (not c) THROW(BadRequest, "(LineRoute) missing correction data");
-	LockedSession session(must_find_session(req));
-	auto conn = must_get_connection();
-	session->assert_permission(conn, pid, Permissions::Write);
-	auto line = session->must_find(conn, pid, p, lid);
-	return correct(conn, *line, *c);
+Route::Response LineRoute::impl(HttpPost, const Request &req, int pid, int p,
+                                int lid) const {
+  const auto json = crow::json::load(req.body);
+  const auto c = get<std::string>(json, "correction");
+  if (not c)
+    THROW(BadRequest, "(LineRoute) missing correction data");
+  LockedSession session(must_find_session(req));
+  auto conn = must_get_connection();
+  session->assert_permission(conn, pid, Permissions::Write);
+  auto line = session->must_find(conn, pid, p, lid);
+  return correct(conn, *line, *c);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response LineRoute::impl(HttpPost, const Request& req, int pid, int p,
-				int lid, int tid) const {
-	const auto json = crow::json::load(req.body);
-	const auto c = get<std::string>(json, "correction");
-	if (not c) THROW(BadRequest, "(LineRoute) missing correction data");
-	LockedSession session(must_find_session(req));
-	auto conn = must_get_connection();
-	session->assert_permission(conn, pid, Permissions::Write);
-	auto line = session->must_find(conn, pid, p, lid);
-	return correct(conn, *line, tid, *c);
+Route::Response LineRoute::impl(HttpGet, const Request &req, int pid, int p,
+                                int lid, int tid) const {
+  LockedSession session(must_find_session(req));
+  auto conn = must_get_connection();
+  auto line = session->must_find(conn, pid, p, lid);
+  auto token = find_token(*line, tid);
+  if (not token) {
+    THROW(NotFound, "(LineRoute) cannot find token id ", tid);
+  }
+  Json j;
+  return j << *token;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response LineRoute::correct(MysqlConnection& conn, Line& line,
-				   const std::string& c) const {
-	WagnerFischer wf;
-	wf.set_gt(c);
-	wf.set_ocr(line);
-	const auto lev = wf();
-	CROW_LOG_DEBUG << "(LineRoute) correction: " << c;
-	CROW_LOG_DEBUG << "(LineRoute) line.cor(): " << line.cor();
-	CROW_LOG_DEBUG << "(LineRoute) line.ocr(): " << line.ocr();
-	CROW_LOG_DEBUG << "(LineRoute)        lev: " << lev;
-	wf.correct(line);
-	update_line(conn, line);
-	Json j;
-	return j << line;
+Route::Response LineRoute::impl(HttpPost, const Request &req, int pid, int p,
+                                int lid, int tid) const {
+  const auto json = crow::json::load(req.body);
+  const auto c = get<std::string>(json, "correction");
+  if (not c)
+    THROW(BadRequest, "(LineRoute) missing correction data");
+  LockedSession session(must_find_session(req));
+  auto conn = must_get_connection();
+  session->assert_permission(conn, pid, Permissions::Write);
+  auto line = session->must_find(conn, pid, p, lid);
+  return correct(conn, *line, tid, *c);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response LineRoute::correct(MysqlConnection& conn, Line& line, int tid,
-				   const std::string& c) const {
-	auto token = find_token(line, tid);
-	if (not token) {
-		THROW(NotFound, "(LineRoute) cannot find ",
-		      line.page().book().id(), "-", line.page().id(), "-",
-		      line.id(), "-", tid);
-	}
-	WagnerFischer wf;
-	wf.set_ocr(line);
-	wf.set_gt(c);
-	const auto b = token->offset();
-	const auto n = token->size();
-	const auto lev = wf(b, n);
-	CROW_LOG_DEBUG << "(LineRoute) correction: " << c;
-	CROW_LOG_DEBUG << "(LineRoute) line.cor(): " << line.cor();
-	CROW_LOG_DEBUG << "(LineRoute) line.ocr(): " << line.ocr();
-	CROW_LOG_DEBUG << "(LineRoute)        lev: " << lev;
-	wf.correct(line, b, n);
-	token = find_token(line, tid);
-	if (not token) {
-		THROW(Error, "(LineRoute) cannot find token ",
-		      line.page().book().id(), "-", line.page().id(), "-",
-		      line.id(), "-", tid, " after correction");
-	}
-	update_line(conn, line);
-	Json j;
-	return j << *token;
+Route::Response LineRoute::correct(MysqlConnection &conn, Line &line,
+                                   const std::string &c) const {
+  WagnerFischer wf;
+  wf.set_gt(c);
+  wf.set_ocr(line);
+  const auto lev = wf();
+  CROW_LOG_DEBUG << "(LineRoute) correction: " << c;
+  CROW_LOG_DEBUG << "(LineRoute) line.cor(): " << line.cor();
+  CROW_LOG_DEBUG << "(LineRoute) line.ocr(): " << line.ocr();
+  CROW_LOG_DEBUG << "(LineRoute)        lev: " << lev;
+  wf.correct(line);
+  update_line(conn, line);
+  Json j;
+  return j << line;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-boost::optional<Token> LineRoute::find_token(const Line& line, int tid) {
-	boost::optional<Token> token;
-	line.each_token([&](const auto& t) {
-		if (t.id == tid) {
-			token = t;
-		}
-	});
-	return token;
+Route::Response LineRoute::correct(MysqlConnection &conn, Line &line, int tid,
+                                   const std::string &c) const {
+  auto token = find_token(line, tid);
+  if (not token) {
+    THROW(NotFound, "(LineRoute) cannot find ", line.page().book().id(), "-",
+          line.page().id(), "-", line.id(), "-", tid);
+  }
+  WagnerFischer wf;
+  wf.set_ocr(line);
+  wf.set_gt(c);
+  const auto b = token->offset();
+  const auto n = token->size();
+  const auto lev = wf(b, n);
+  CROW_LOG_DEBUG << "(LineRoute) correction: " << c;
+  CROW_LOG_DEBUG << "(LineRoute) line.cor(): " << line.cor();
+  CROW_LOG_DEBUG << "(LineRoute) line.ocr(): " << line.ocr();
+  CROW_LOG_DEBUG << "(LineRoute)        lev: " << lev;
+  wf.correct(line, b, n);
+  token = find_token(line, tid);
+  if (not token) {
+    THROW(Error, "(LineRoute) cannot find token ", line.page().book().id(), "-",
+          line.page().id(), "-", line.id(), "-", tid, " after correction");
+  }
+  update_line(conn, line);
+  Json j;
+  return j << *token;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void LineRoute::update_line(MysqlConnection& conn, const Line& line) {
-	MysqlCommiter commiter(conn);
-	pcw::update_line(conn.db(), line);
-	commiter.commit();
+boost::optional<Token> LineRoute::find_token(const Line &line, int tid) {
+  boost::optional<Token> token;
+  line.each_token([&](const auto &t) {
+    if (t.id == tid) {
+      token = t;
+    }
+  });
+  return token;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void LineRoute::update_line(MysqlConnection &conn, const Line &line) {
+  MysqlCommiter commiter(conn);
+  pcw::update_line(conn.db(), line);
+  commiter.commit();
 }
--- rest/src/api/LineRoute.hpp
+++ rest/src/api/LineRoute.hpp
@@ -6,31 +6,31 @@
 namespace pcw {
 struct Token;
 class LineRoute : public CrtpRoute<LineRoute> {
-       public:
-	virtual ~LineRoute() noexcept override = default;
-	virtual void Register(App& app) override;
-	virtual const char* route() const noexcept override { return route_; }
-	virtual const char* name() const noexcept override { return name_; }
+public:
+  virtual ~LineRoute() noexcept override = default;
+  virtual void Register(App &app) override;
+  virtual const char *route() const noexcept override { return route_; }
+  virtual const char *name() const noexcept override { return name_; }
 
-	pcw_crtp_route_def_impl__(int, int, int);
-	pcw_crtp_route_def_impl__(int, int, int, int);
-	Response impl(HttpGet, const Request& req, int bid, int pid,
-		      int lid) const;
-	Response impl(HttpPost, const Request& req, int bid, int pid,
-		      int lid) const;
-	Response impl(HttpPost, const Request& req, int pid, int p, int lid,
-		      int tid) const;
+  pcw_crtp_route_def_impl__(int, int, int);
+  pcw_crtp_route_def_impl__(int, int, int, int);
+  Response impl(HttpGet, const Request &req, int bid, int pid, int lid) const;
+  Response impl(HttpPost, const Request &req, int bid, int pid, int lid) const;
+  Response impl(HttpGet, const Request &req, int pid, int p, int lid,
+                int tid) const;
+  Response impl(HttpPost, const Request &req, int pid, int p, int lid,
+                int tid) const;
 
-       private:
-	Response correct(MysqlConnection& conn, Line& line,
-			 const std::string& c) const;
-	Response correct(MysqlConnection& conn, Line& line, int tid,
-			 const std::string& c) const;
-	static boost::optional<Token> find_token(const Line& line, int tid);
-	static void update_line(MysqlConnection& conn, const Line& line);
-	static const char* route_;
-	static const char* name_;
+private:
+  Response correct(MysqlConnection &conn, Line &line,
+                   const std::string &c) const;
+  Response correct(MysqlConnection &conn, Line &line, int tid,
+                   const std::string &c) const;
+  static boost::optional<Token> find_token(const Line &line, int tid);
+  static void update_line(MysqlConnection &conn, const Line &line);
+  static const char *route_;
+  static const char *name_;
 };
 }
 
-#endif  // pcw_LineRoute_hpp__
+#endif // pcw_LineRoute_hpp__
