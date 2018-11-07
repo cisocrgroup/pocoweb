758b9675330ca02a9f5908e01482417572e2418c
flo <flo@cis.lmu.de>
2018-11-07 21:09:00 +0100

    Update internalURL in frontend configuration

--- frontend/resources/config.php
+++ frontend/resources/config.php
@@ -19,7 +19,7 @@ $config = array(
 		),
 	),
 	"backend" => array(
-	  "internalURL"    => "http://pocoweb:8080",
+	  "internalURL"    => "http://pcwauth:8081",
 		"url"            => "http://localhost/rest",
 		"profilerWaitMs" => 30000,
 		"routes"         => array(
