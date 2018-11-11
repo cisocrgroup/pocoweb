f1370839c28ff4b2ed34bb03e1acc797de93691e
flo <flo@cis.lmu.de>
2018-11-11 14:31:13 +0100

    Fix handling of admin account

--- frontend/resources/library/frontend.php
+++ frontend/resources/library/frontend.php
@@ -311,14 +311,13 @@ function frontend_upload_project_archive($post, $file) {
 	}
 }
 
-function frontend_render_users_div() {
-	global $USER;
-	if ($USER != NULL && $USER["admin"]) {
-		frontend_render_create_new_user_div();
-		frontend_render_users_table_div();
-	} else {
-		frontend_render_warning_div("You are not allowed to do user administration");
-	}
+function frontend_render_users_div($user) {
+    if (!$user['admin']) {
+        frontend_render_warning_div("You are not allowed to do user administration");
+        return;
+    }
+    frontend_render_create_new_user_div();
+    frontend_render_users_table_div();
 }
 
 function frontend_render_create_new_user_div() {
