469bdd1e6d6f9ee67cae808cc6891599c147ceab
flo <flo@cis.lmu.de>
2018-11-13 16:10:00 +0100

    Add final ; to generated config.js

--- frontend/resources/genconfig.js.php
+++ frontend/resources/genconfig.js.php
@@ -2,5 +2,5 @@
 require_once(dirname(__FILE__) . "/config.php");
 global $config;
 echo 'var pcw = {}', ";\n";
-echo 'pcw.config = ', json_encode($config,JSON_PRETTY_PRINT), "\n";
+echo 'pcw.config = ', json_encode($config,JSON_PRETTY_PRINT), ";\n";
 ?>
