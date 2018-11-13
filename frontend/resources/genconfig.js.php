e18afa098a94f931f77ecdd8ffc9d55db67eb608
flo <flo@cis.lmu.de>
2018-11-13 13:35:46 +0100

    Fix newline output

--- frontend/resources/genconfig.js.php
+++ frontend/resources/genconfig.js.php
@@ -1,6 +1,6 @@
 <?php
 require_once(dirname(__FILE__) . "/config.php");
 global $config;
-echo 'var pcw = {};';
-echo 'pcw.config = ', json_encode($config), ';';
+echo 'var pcw = {}', ";\n";
+echo 'pcw.config = ', json_encode($config,JSON_PRETTY_PRINT), "\n";
 ?>
