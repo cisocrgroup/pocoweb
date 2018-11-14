a104628318117b5b6a02ed598a9e2ec98a289cff
flo <flo@cis.lmu.de>
2018-11-14 23:30:23 +0100

    Update profiler and pocoweb to use fedora (ubuntu seems broken)

--- misc/docker/env.sh
+++ misc/docker/env.sh
@@ -9,8 +9,3 @@ MYSQL_DATABASE=pocoweb
 PCW_DB_HOST=http://db:3306
 PCW_POCOWEB_HOST=http://pocoweb:8080
 PCW_PCWAUTH_HOST=:8081
-
-    # environment:
-    #   - PCWAUTH_DB=pocoweb:pocoweb1998@(db)/pocoweb
-    #   - PCWAUTH_POCOWEB=http://pocoweb:8080
-    #   - PCWAUTH_HOST=:8081
