4d673bd711873cc84abdbeba8fbb97f6f0b2a6f7
flo <flo@cis.lmu.de>
2018-11-23 15:49:46 +0100

    Handle utf8 in database and improve general layout

--- misc/docker/env.sh
+++ misc/docker/env.sh
@@ -1,11 +1,16 @@
 #/bin/sh
 
-# MYSQL DATA
+# Settings for the mysql user
 MYSQL_ROOT_PASSWORD=example
 MYSQL_USER=pocoweb
 MYSQL_PASSWORD=pocoweb1998
 MYSQL_DATABASE=pocoweb
-
+# Misc settings
 PCW_DB_HOST=http://db:3306
 PCW_POCOWEB_HOST=http://pocoweb:8080
 PCW_PCWAUTH_HOST=:8081
+# Settings for the root user of pocoweb
+PCW_ROOT_NAME=root
+PCW_ROOT_EMAIL=root@example.com
+PCW_ROOT_PASSWORD=password
+PCW_ROOT_INSTITUTE=CIS
