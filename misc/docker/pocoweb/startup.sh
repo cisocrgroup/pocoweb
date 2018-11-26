b76722cab9f2bc03468c113d3478edab01d1e42d
flo <flo@cis.lmu.de>
2018-11-26 10:50:18 +0100

    Copy web files from /app/{resource,public_html} to /www-data on startup

--- misc/docker/pocoweb/startup.sh
+++ misc/docker/pocoweb/startup.sh
@@ -2,11 +2,16 @@
 
 config=/apps/pocoweb.conf
 sql=/apps/tables.sql
+public_html=/apps/public_html
+resoures=/apps/resources
 
 user=$(git config -f $config --get db.user)
 pass=$(git config -f $config --get db.pass)
 host=$(git config -f $config --get db.host)
 name=$(git config -f $config --get db.name)
 
+cp -vr $public_html /www-data
+cp -vr $resoures /www-data
+sleep 5
 mysql -h$host -u$user -p$pass $name < $sql || exit 1
 /apps/pocoweb $config || exit 1
