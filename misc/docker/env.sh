2e26ae59399697102d7cea29d7a8ac7007c17d3d
flo <flo@cis.lmu.de>
2018-12-03 14:09:04 +0100

    Do use env.sh variables

--- misc/docker/env.sh
+++ misc/docker/env.sh
@@ -1,7 +1,11 @@
 #/bin/sh
 
 # Base dirctory of volumes
-PCW_BASE_DIR=/home/flo/data/srv/pocoweb
+#
+# Set here or use export
+# export PCW_BASE_DIR=/srv/pocoweb
+# docker-compose ...
+#PCW_BASE_DIR
 
 # Settings for the mysql user
 MYSQL_ROOT_PASSWORD=example
@@ -9,7 +13,7 @@ MYSQL_USER=pocoweb
 MYSQL_PASSWORD=pocoweb1998
 MYSQL_DATABASE=pocoweb
 # Misc settings
-PCW_DB_HOST=http://db:3306
+PCW_DB_HOST=db
 PCW_POCOWEB_HOST=http://pocoweb:8080
 PCW_PCWAUTH_HOST=:8081
 # Settings for the root user of pocoweb
--- misc/docker/pocoweb/startup.sh
+++ misc/docker/pocoweb/startup.sh
@@ -5,13 +5,9 @@ sql=/apps/tables.sql
 public_html=/apps/public_html
 resoures=/apps/resources
 
-user=$(git config -f $config --get db.user)
-pass=$(git config -f $config --get db.pass)
-host=$(git config -f $config --get db.host)
-name=$(git config -f $config --get db.name)
-
 cp -vr $public_html /www-data
 cp -vr $resoures /www-data
 sleep 10
-mysql -h$host -u$user -p$pass $name < $sql || exit 1
+
+mysql -h$PCW_DB_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < $sql || exit 1
 /apps/pocoweb $config || exit 1
