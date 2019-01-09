e73042980a7e98a3e9803380560437141a565998
flo <flo@cis.lmu.de>
2019-01-09 15:50:13 +0100

    Indentation

--- rest/src/parser/Xml.hpp
+++ rest/src/parser/Xml.hpp
@@ -5,31 +5,32 @@
 #include <memory>
 
 namespace pugi {
-	class xml_node;
-	class xml_document;
+class xml_node;
+class xml_document;
 }
 
 namespace pcw {
-	using Path = boost::filesystem::path;
-	class Xml {
-	public:
-		using Doc = pugi::xml_document;
-		using Node = pugi::xml_node;
-		using DocPtr = std::shared_ptr<pugi::xml_document>;
+using Path = boost::filesystem::path;
+class Xml
+{
+public:
+  using Doc = pugi::xml_document;
+  using Node = pugi::xml_node;
+  using DocPtr = std::shared_ptr<pugi::xml_document>;
 
-		Xml(DocPtr doc = nullptr);
-		Xml(const Path& path);
-		virtual ~Xml() noexcept = default;
-		Doc& doc() noexcept {return *doc_;}
-		const Doc& doc() const noexcept {return *doc_;}
-		DocPtr& doc_ptr() noexcept {return doc_;}
-		const DocPtr& doc_ptr() const noexcept {return doc_;}
-		void read(const Path& path);
-		void write(const Path& path) const;
+  Xml(DocPtr doc = nullptr);
+  Xml(const Path& path);
+  virtual ~Xml() noexcept = default;
+  Doc& doc() noexcept { return *doc_; }
+  const Doc& doc() const noexcept { return *doc_; }
+  DocPtr& doc_ptr() noexcept { return doc_; }
+  const DocPtr& doc_ptr() const noexcept { return doc_; }
+  void read(const Path& path);
+  void write(const Path& path) const;
 
-	private:
-		DocPtr doc_;
-	};
+private:
+  DocPtr doc_;
+};
 }
 
 #endif // pcw_Xml_hpp__
