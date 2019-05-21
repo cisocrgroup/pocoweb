0b29326d44fe43e85a1ab7d8972f823cb491856e
flo <flo@cis.lmu.de>
2019-05-21 16:33:45 +0200

    No verbose copying

--- misc/docker/pocoweb/startup.sh
+++ misc/docker/pocoweb/startup.sh
@@ -5,9 +5,8 @@ sql=/apps/tables.sql
 public_html=/apps/public_html
 resoures=/apps/resources
 
-cp -vr $public_html /www-data
-cp -vr $resoures /www-data
+cp -r $public_html /www-data
+cp -r $resoures /www-data
 sleep 10
-
 mysql -h$PCW_DB_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < $sql || exit 1
 /apps/pocoweb $config || exit 1
