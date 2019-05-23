f2ce5c73a5bf560c093da33d539f78e684aa0c2e
flo <flo@cis.lmu.de>
2019-05-23 14:53:27 +0200

    Finalize implementation of pattern and word search

--- rest/src/api/SearchRoute.cpp
+++ rest/src/api/SearchRoute.cpp
@@ -23,8 +23,8 @@
 using namespace pcw;
 
 template <class M>
-static void make_response(Json &json, const M &matches, int bookid,
-                          const std::string &q, bool ep);
+static void add_matches(Json &json, const M &matches, const std::string &q,
+                        bool ep);
 
 ////////////////////////////////////////////////////////////////////////////////
 const char *SearchRoute::route_ = SEARCH_ROUTE_ROUTE;
@@ -54,20 +54,20 @@ Route::Response SearchRoute::search(const Request &req,
                                     const std::vector<std::string> &qs, int bid,
                                     TokenQuery) const {
   const LockedSession session(get_session(req));
+  auto conn = must_get_connection();
+  const auto project = session->must_find(conn, bid);
   Json json;
+  json["matches"] = crow::json::rvalue(crow::json::type::Object);
   json["projectId"] = bid;
   json["isErrorPattern"] = false;
   for (const auto &q : qs) {
     CROW_LOG_DEBUG << "(SearchRoute::search) searching project id: " << bid
                    << " for query string q: " << q;
-
-    auto conn = must_get_connection();
-    const auto project = session->must_find(conn, bid);
     Searcher searcher(*project);
     const auto matches = searcher.find(q);
     CROW_LOG_DEBUG << "(SearchRoute::search) found " << matches.size()
                    << " matches for q='" << q << "'";
-    make_response(json, matches, bid, q, false);
+    add_matches(json, matches, q, false);
   }
   return json;
 }
@@ -76,37 +76,43 @@ Route::Response SearchRoute::search(const Request &req,
 Route::Response SearchRoute::search(const Request &req,
                                     const std::vector<std::string> &qs, int bid,
                                     ErrorPatternQuery) const {
-  CROW_LOG_DEBUG << "(SearchRoute::search) searching project id: " << bid
-                 << " for error pattern q: " << qs[0];
-  // const LockedSession session(get_session(req));
-  // auto conn = must_get_connection();
-  // const auto project = session->must_find(conn, bid);
-  // tables::Errorpatterns e;
-  // tables::Suggestions s;
-  // auto rows =
-  //     conn.db()(select(s.pageid, s.lineid, s.tokenid)
-  //                   .from(s.join(e).on(s.suggestiontypid ==
-  //                   e.suggestiontypid)) .where(e.bookid == bid and e.pattern
-  //                   == q));
-  // std::set<std::tuple<int, int, int>> ids;
-  // for (const auto &row : rows) {
-  //   ids.emplace(row.pageid, row.lineid, row.tokenid);
-  // }
-  // Searcher searcher(*project);
-  // const auto matches = searcher.find_impl([&ids](const auto &t) {
-  //   auto p = std::make_tuple(t.line->page().id(), t.line->id(), t.id);
-  //   return ids.count(p);
-  // });
-  // CROW_LOG_DEBUG << "(SearchRoute::search) found " << matches.size()
-  //                << " matches for q='" << q << "'";
-  // return make_response(matches, bid, q, true);
-  THROW(BadRequest, "(SearchRoute) pattern search currently not implemented");
+  const LockedSession session(get_session(req));
+  auto conn = must_get_connection();
+  const auto project = session->must_find(conn, bid);
+  std::vector<std::string> tokens;
+  for (const auto &q : qs) {
+    CROW_LOG_DEBUG << "(SearchRoute::search) searching project id: " << bid
+                   << " for error pattern: " << q;
+    tables::Errorpatterns e;
+    tables::Suggestions s;
+    tables::Types t;
+    auto rows = conn.db()(select(t.typ)
+                              .from(e.join(s)
+                                        .on(e.suggestionid == s.id)
+                                        .join(t)
+                                        .on(t.id == s.tokentypid))
+                              .where(e.pattern == q));
+    for (const auto &row : rows) {
+      tokens.push_back(row.typ);
+    }
+  }
+  Json json;
+  json["matches"] = crow::json::rvalue(crow::json::type::Object);
+  json["projectId"] = bid;
+  json["isErrorPattern"] = false;
+  for (const auto t : tokens) {
+    Searcher searcher(*project);
+    const auto matches = searcher.find(t);
+    CROW_LOG_DEBUG << "(SearchRoute::search) found " << matches.size()
+                   << " matches for q='" << t << "'";
+    add_matches(json, matches, t, false);
+  }
+  return json;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 template <class M>
-void make_response(Json &json, const M &matches, int bookid,
-                   const std::string &q, bool ep) {
+void add_matches(Json &json, const M &matches, const std::string &q, bool ep) {
   CROW_LOG_DEBUG << "(SearchRoute::search) building response";
   size_t i = 0;
   for (const auto &m : matches) {
