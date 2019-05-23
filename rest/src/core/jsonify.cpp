2a0df138183cbd40633cc272098d506425a6d3f0
flo <flo@cis.lmu.de>
2019-05-23 16:57:19 +0200

    Fix spelling of json field

--- rest/src/core/jsonify.cpp
+++ rest/src/core/jsonify.cpp
@@ -22,8 +22,7 @@ pcw::Json &pcw::operator<<(Json &json, const BookData &data) {
   json["title"] = data.title;
   json["year"] = data.year;
   json["language"] = data.lang;
-  std::cout << "STATUS: " << data.status << "\n";
-  json["staus"] = data.status;
+  json["status"] = data.status;
   json["description"] = data.description;
   json["profilerUrl"] = data.profilerUrl;
   return json;
