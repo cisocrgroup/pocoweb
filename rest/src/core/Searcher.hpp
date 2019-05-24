104e08af7b5e8b1bfaac8f5bde63a0530d953473
flo <flo@cis.lmu.de>
2019-05-24 14:53:04 +0200

    Indentation

--- rest/src/core/Searcher.hpp
+++ rest/src/core/Searcher.hpp
@@ -9,48 +9,40 @@
 #include <vector>
 
 namespace pcw {
-class Searcher
-{
+class Searcher {
 public:
   using ConstLineSptr = std::shared_ptr<const Line>;
   using Matches = std::map<ConstLineSptr, std::vector<Token>>;
-  Searcher()
-    : Searcher(nullptr)
-  {}
-  Searcher(const Project& project);
+  Searcher() : Searcher(nullptr) {}
+  Searcher(const Project &project);
 
   void set_ignore_case(bool ic = true) { ignore_case_ = ic; }
   bool ignore_case() const noexcept { return ignore_case_; }
-  void set_project(const Project& project) noexcept;
-  Matches find(const std::wstring& str) const;
-  Matches find(const std::string& str) const;
+  void set_project(const Project &project) noexcept;
+  Matches find(const std::wstring &str) const;
+  Matches find(const std::string &str) const;
 
-  template<class F>
-  Matches find_impl(F f) const;
+  template <class F> Matches find_impl(F f) const;
 
 private:
   Searcher(std::shared_ptr<const Project> p)
-    : project_(std::move(p))
-    , ignore_case_(true)
-  {}
+      : project_(std::move(p)), ignore_case_(true) {}
   std::shared_ptr<const Project> project_;
   bool ignore_case_;
 };
-}
+} // namespace pcw
 
 ////////////////////////////////////////////////////////////////////////////////
-template<class F>
-inline pcw::Searcher::Matches
-pcw::Searcher::find_impl(F f) const
-{
+template <class F>
+inline pcw::Searcher::Matches pcw::Searcher::find_impl(F f) const {
   if (not project_)
     return {};
   Matches matches;
-  for (const auto& page : *project_) {
+  for (const auto &page : *project_) {
     if (page) {
-      for (const auto& line : *page) {
+      for (const auto &line : *page) {
         if (line) {
-          line->each_token([&](const auto& t) {
+          line->each_token([&](const auto &t) {
             if (f(t))
               matches[line].push_back(t);
           });
