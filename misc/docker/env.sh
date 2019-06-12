edd0120441d135045379ee1b842aac62b260eca3
flo <flo@cis.lmu.de>
2019-06-12 11:07:57 +0200

    Pocoweb container uses normal unexposed 80 port

--- misc/docker/env.sh
+++ misc/docker/env.sh
@@ -14,8 +14,8 @@ MYSQL_PASSWORD=pocoweb1998
 MYSQL_DATABASE=pocoweb
 # Misc settings
 PCW_DB_HOST=db
-PCW_POCOWEB_HOST=http://pocoweb:8080
-PCW_PCWAUTH_HOST=:8081
+# PCW_POCOWEB_HOST=http://pocoweb:8080
+# PCW_PCWAUTH_HOST=:8081
 # Settings for the root user of pocoweb
 PCW_ROOT_NAME=root
 PCW_ROOT_EMAIL=root@example.com
