98acf00c706e59dd64aa2ee981cda5a3cc11ee8b
flo <flo@cis.lmu.de>
2019-06-19 13:35:57 +0200

    Fix include

--- frontend/public_html/api/api.php
+++ frontend/public_html/api/api.php
@@ -1,6 +1,6 @@
 <?php
-require_once(dirname(dirname(__FILE__)) . "/config.php");
-require_once(LIBRARY_PATH . "/utils.php");
+require_once("./config.php");
+require_once("./utils.php");
 
 class Api {
 	public function __construct($url) {
