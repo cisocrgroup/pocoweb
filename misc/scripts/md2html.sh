37e394a03e3ab34e5d37abff433faab40986c5a0
flo <flo@cis.lmu.de>
2019-07-25 13:28:30 +0200

    Apply custom css for json in documentation

--- misc/scripts/md2html.sh
+++ misc/scripts/md2html.sh
@@ -5,5 +5,7 @@ text=${text//	/\\\t}
 text=${text//
 /\\\n}
 
-curl -Ss https://api.github.com/markdown -d "{\"text\":\"$text\",\"mode\":\"markdown\",\"context\":\"\"}" || exit 1
+curl -Ss https://api.github.com/markdown -d "{\"mode\":\"gfm\",\"text\":\"$text\",\"mode\":\"markdown\",\"context\":\"\"}" \
+	| sed -e 's#<span class="pl-c1">false</span>#<span class="pl-bl">false</span>#' \
+		  -e 's#<span class="pl-c1">true</span>#<span class="pl-bl">true</span>#'
 echo ""
