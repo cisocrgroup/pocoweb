065da8661e7ac379ab4380c8355f4cc0e8f966bc
flo <flo@cis.lmu.de>
2019-07-30 14:40:22 +0200

    Split and assign packages

--- rest/src/api/SplitRoute.cpp
+++ rest/src/api/SplitRoute.cpp
@@ -1,5 +1,4 @@
 #include "SplitRoute.hpp"
-#include "core/Archiver.hpp"
 #include "core/Book.hpp"
 #include "core/Package.hpp"
 #include "core/Page.hpp"
@@ -28,83 +27,123 @@ void SplitRoute::Register(App &app) {
 
 ////////////////////////////////////////////////////////////////////////////////
 Route::Response SplitRoute::impl(HttpPost, const Request &req, int bid) const {
-  return split(req, bid);
+  CROW_LOG_INFO << "(SplitRoute) body: " << req.body;
+  auto data = crow::json::load(req.body);
+  const auto random = get<bool>(data, "random").value_or(false);
+  const auto ids = get<std::vector<int>>(data, "ids");
+  if (not ids.has_value() or ids.value().empty()) {
+    THROW(BadRequest,
+          "(SplitRoute) cannot split package: missing or empty ids");
+  }
+  auto conn = must_get_connection();
+  Splitter splitter(conn, bid, random);
+  return splitter.toJSON(splitter.split(ids.value()));
+
+  // auto proj = session->must_find(conn, bid);
+  // if (not proj->is_book())
+  //   const auto n = data["n"].i();
+  // if (n <= 0 or static_cast<size_t>(n) > proj->size())
+  //   THROW(BadRequest, "cannot split book: invalid split number: ", n);
+  // std::vector<ProjectPtr> projs(n);
+  // std::generate(begin(projs), end(projs), [&]() {
+  //   return std::make_shared<Package>(0, session->user(), proj->origin());
+  // });
+  // // project is a book so it's origin() points to itself
+  // if (data.has("random") and data["random"].b())
+  //   split_random(proj->origin(), projs);
+  // else
+  //   split_sequencial(proj->origin(), projs);
+  // MysqlCommiter commiter(conn);
+  // for (const auto &p : projs) {
+  //   assert(p);
+  //   insert_project(conn.db(), *p);
+  // }
+  // commiter.commit();
+  // Json j;
+  // return j << projs;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response SplitRoute::split(const Request &req, int bid) const {
-  CROW_LOG_DEBUG << "(SplitRoute::split) body: " << req.body;
-  auto data = crow::json::load(req.body);
-  auto conn = must_get_connection();
-  LockedSession session(get_session(req));
-  auto proj = session->must_find(conn, bid);
-  if (not proj->is_book())
-    THROW(BadRequest, "cannot split project: only books can be split");
-  if (proj->owner() != session->user())
-    THROW(Forbidden, "cannot split book: permission denied");
-  const auto n = data["n"].i();
-  if (n <= 0 or static_cast<size_t>(n) > proj->size())
-    THROW(BadRequest, "cannot split book: invalid split number: ", n);
-  std::vector<ProjectPtr> projs(n);
-  std::generate(begin(projs), end(projs), [&]() {
-    return std::make_shared<Package>(0, session->user(), proj->origin());
-  });
-  // project is a book so it's origin() points to itself
-  if (data.has("random") and data["random"].b())
-    split_random(proj->origin(), projs);
-  else
-    split_sequencial(proj->origin(), projs);
-  MysqlCommiter commiter(conn);
-  for (const auto &p : projs) {
-    assert(p);
-    insert_project(conn.db(), *p);
+std::unordered_map<int, std::vector<int>>
+Splitter::split(const std::vector<int> &userids) {
+  using namespace sqlpp;
+  tables::Projects p;
+  auto rows = c_.db()(select(all_of(p)).from(p).where(p.id == pid_));
+  if (rows.empty()) {
+    THROW(NotFound, "(Splitter) cannot split project: not found");
+  }
+  if (rows.front().origin != rows.front().id) { // not a project
+    THROW(BadRequest,
+          "(Splitter) cannot split package: only projects can be split");
+  }
+  owner_ = rows.front().owner;
+  const auto projs = genProjects(userids.size());
+  return assign(userids, projs);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+std::unordered_map<int, std::vector<int>>
+Splitter::assign(const std::vector<int> &userids,
+                 const std::vector<std::vector<int>> &projs) const {
+  assert(userids.size() == projs.size());
+  MysqlCommiter commiter(c_);
+  using namespace sqlpp;
+  tables::ProjectPages pp;
+  tables::Projects p;
+  auto insertp = c_.db().prepare(
+      insert_into(p).set(p.origin = pid_, p.owner = parameter(p.owner),
+                         p.pages = parameter(p.pages)));
+  auto insertpp = c_.db().prepare(
+      insert_into(pp).set(pp.projectid = parameter(pp.projectid),
+                          pp.pageid = parameter(pp.pageid)));
+  std::unordered_map<int, std::vector<int>> projmap;
+  for (auto i = 0U; i < userids.size(); i++) {
+    insertp.params.pages = static_cast<int>(projs[i].size());
+    const auto pid = c_.db()(insertp);
+    for (const auto pageid : projs[i]) {
+      insertpp.params.projectid = pid;
+      insertpp.params.pageid = pageid;
+      c_.db()(insertpp);
+      projmap[pid].push_back(pageid);
+    }
   }
   commiter.commit();
-  Json j;
-  return j << projs;
+  return projmap;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void SplitRoute::split_random(const Book &book,
-                              std::vector<ProjectPtr> &projs) const {
-  assert(projs.size() <= book.size());
-  std::vector<PagePtr> tmp_pages(book.size());
-  std::copy(begin(book), end(book), begin(tmp_pages));
-  // std::random_device rd;
-  std::mt19937 g(genseed());
-  std::shuffle(begin(tmp_pages), end(tmp_pages), g);
-  const auto n = projs.size();
-  for (auto i = 0U; i < tmp_pages.size(); ++i) {
-    assert(projs[i % n]);
-    projs[i % n]->push_back(*tmp_pages[i]);
+std::vector<std::vector<int>> Splitter::genProjects(size_t n) const {
+  using namespace sqlpp;
+  tables::ProjectPages p;
+  auto rows = c_.db()(select(all_of(p)).from(p).where(p.projectid == pid_));
+  std::vector<int> pageids;
+  for (const auto &row : rows) {
+    pageids.push_back(row.pageid);
   }
-  for (auto &p : projs) {
-    assert(p);
-    std::sort(begin(*p), end(*p), [](const auto &a, const auto &b) {
-      assert(a);
-      assert(b);
-      return a->id() < b->id();
-    });
+  if (random_) {
+    std::mt19937 g(genseed());
+    std::shuffle(begin(pageids), end(pageids), g);
   }
-}
-
-////////////////////////////////////////////////////////////////////////////////
-void SplitRoute::split_sequencial(const Book &book,
-                                  std::vector<ProjectPtr> &projs) const {
-  assert(projs.size() <= book.size());
-  const auto n = projs.size();
-  const auto d = book.size() / n;
-  auto r = book.size() % n;
-  std::vector<PagePtr> tmp_pages(book.size());
-  std::copy(begin(book), end(book), begin(tmp_pages));
-  size_t o = 0;
+  std::vector<std::vector<int>> projs(n);
+  const auto d = pageids.size() / n;
+  auto r = pageids.size() % n;
+  auto o = 0U;
   for (auto i = 0U; i < n; ++i) {
-    assert(projs[i]);
     for (auto j = 0U; j < (d + r); ++j) {
-      projs[i]->push_back(*tmp_pages[j + o]);
+      projs[i].push_back(pageids[j + o]);
     }
     o += (d + r);
-    if (r > 0)
+    if (r > 0) {
       --r;
+    }
+    std::sort(projs[i].begin(), projs[i].end());
   }
+  return projs;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+Json Splitter::toJSON(
+    const std::unordered_map<int, std::vector<int>> &projs) const {
+  Json j;
+  return j;
 }
--- rest/src/api/SplitRoute.hpp
+++ rest/src/api/SplitRoute.hpp
@@ -2,32 +2,47 @@
 #define pcw_SplitRoute_hpp__
 
 #include "core/CrtpRoute.hpp"
-
+namespace crow {
+namespace json {
+class wvalue;
+} // namespace json
+} // namespace crow
 namespace pcw {
 class Book;
 class Project;
 using ProjectPtr = std::shared_ptr<Project>;
+using Json = crow::json::wvalue;
 
 class SplitRoute : public CrtpRoute<SplitRoute> {
-       public:
-	virtual ~SplitRoute() noexcept override = default;
-	virtual void Register(App& app) override;
-	virtual const char* route() const noexcept override { return route_; }
-	virtual const char* name() const noexcept override { return name_; }
+public:
+  virtual ~SplitRoute() noexcept override = default;
+  virtual void Register(App &app) override;
+  virtual const char *route() const noexcept override { return route_; }
+  virtual const char *name() const noexcept override { return name_; }
 
-	pcw_crtp_route_def_impl__(int);
-	Response impl(HttpPost, const Request& req, int bid) const;
+  pcw_crtp_route_def_impl__(int);
+  Response impl(HttpPost, const Request &req, int bid) const;
 
-       private:
-	Response split(const Request& req, int bid) const;
-	void split_random(const Book& book,
-			  std::vector<ProjectPtr>& projs) const;
-	void split_sequencial(const Book& book,
-			      std::vector<ProjectPtr>& projs) const;
-
-	static const char* route_;
-	static const char* name_;
+private:
+  static const char *route_;
+  static const char *name_;
 };
-}
 
-#endif  // pcw_SplitRoute_hpp__
+class Splitter {
+public:
+  Splitter(MysqlConnection &c, int pid, bool random)
+      : c_(c), pid_(pid), owner_(), random_(random) {}
+  std::unordered_map<int, std::vector<int>> split(const std::vector<int> &ids);
+  Json toJSON(const std::unordered_map<int, std::vector<int>> &projs) const;
+
+private:
+  std::vector<std::vector<int>> genProjects(size_t n) const;
+  std::unordered_map<int, std::vector<int>>
+  assign(const std::vector<int> &ids,
+         const std::vector<std::vector<int>> &projs) const;
+  MysqlConnection &c_;
+  int pid_, owner_;
+  bool random_;
+};
+} // namespace pcw
+#endif // pcw_SplitRoute_hpp__
