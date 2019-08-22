40d89bba4dab77f331e0c14cc3729ef4a16037fa
finkf <finkf@cis.lmu.de>
2019-08-22 14:33:53 +0200

    Indentation

--- rest/src/api/LineRoute.cpp
+++ rest/src/api/LineRoute.cpp
@@ -11,24 +11,21 @@
 using namespace pcw;
 
 ////////////////////////////////////////////////////////////////////////////////
-const char* LineRoute::route_ = LINE_ROUTE_ROUTE "," WORD_ROUTE_ROUTE;
-const char* LineRoute::name_ = "LineRoute";
+const char *LineRoute::route_ = LINE_ROUTE_ROUTE "," WORD_ROUTE_ROUTE;
+const char *LineRoute::name_ = "LineRoute";
 
 ////////////////////////////////////////////////////////////////////////////////
-void
-LineRoute::Register(App& app)
-{
+void LineRoute::Register(App &app) {
   CROW_ROUTE(app, LINE_ROUTE_ROUTE)
-    .methods("GET"_method, "POST"_method, "DELETE"_method)(*this);
+      .methods("GET"_method, "POST"_method, "DELETE"_method)(*this);
   CROW_ROUTE(app, WORD_ROUTE_ROUTE).methods("GET"_method, "POST"_method)(*this);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // GET /books/<bid>/pages/<pid>/lines/<lid>
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response
-LineRoute::impl(HttpGet, const Request& req, int pid, int pageid, int lid) const
-{
+Route::Response LineRoute::impl(HttpGet, const Request &req, int pid,
+                                int pageid, int lid) const {
   CROW_LOG_INFO << "(LineRoute) GET line: " << pid << ":" << pageid << ":"
                 << lid;
   auto conn = must_get_connection();
@@ -43,9 +40,8 @@ LineRoute::impl(HttpGet, const Request& req, int pid, int pageid, int lid) const
 ////////////////////////////////////////////////////////////////////////////////
 // POST /books/<bid>/pages/<pid>/lines/<lid>
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response
-LineRoute::impl(HttpPost, const Request& req, int pid, int p, int lid) const
-{
+Route::Response LineRoute::impl(HttpPost, const Request &req, int pid, int p,
+                                int lid) const {
   CROW_LOG_INFO << "(LineRoute) POST line: " << pid << ":" << p << ":" << lid;
   const auto json = crow::json::load(req.body);
   const auto correction = get<std::string>(json, "correction");
@@ -66,9 +62,8 @@ LineRoute::impl(HttpPost, const Request& req, int pid, int p, int lid) const
 ////////////////////////////////////////////////////////////////////////////////
 // DELETE /books/<bid>/pages/<pid>/lines/<lid>
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response
-LineRoute::impl(HttpDelete, const Request& req, int pid, int p, int lid) const
-{
+Route::Response LineRoute::impl(HttpDelete, const Request &req, int pid, int p,
+                                int lid) const {
   CROW_LOG_INFO << "(LineRoute) DELETE line: " << pid << ":" << p << ":" << lid;
   auto conn = must_get_connection();
   DbLine line(pid, p, lid);
@@ -94,19 +89,13 @@ LineRoute::impl(HttpDelete, const Request& req, int pid, int p, int lid) const
 ////////////////////////////////////////////////////////////////////////////////
 // GET /books/<bid>/pages/<pid>/lines/<lid>/tokens/<tid>{?len=}
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response
-LineRoute::impl(HttpGet,
-                const Request& req,
-                int pid,
-                int pageid,
-                int lid,
-                int tid) const
-{
+Route::Response LineRoute::impl(HttpGet, const Request &req, int pid,
+                                int pageid, int lid, int tid) const {
   CROW_LOG_INFO << "(LineRoute) GET token: " << pid << ":" << pageid << ":"
                 << lid << ":" << tid;
   const auto len = get<int>(req.url_params, "len");
   auto conn = must_get_connection();
-  DbLine line{ pid, pageid, lid };
+  DbLine line{pid, pageid, lid};
   if (not line.load(conn)) {
     THROW(NotFound, "(LineRoute) cannot find ", pid, ":", pageid, ":", lid);
   }
@@ -120,10 +109,8 @@ LineRoute::impl(HttpGet,
 ////////////////////////////////////////////////////////////////////////////////
 // POST /books/<bid>/pages/<pid>/lines/<lid>/tokens/<tid>{?len=}
 ////////////////////////////////////////////////////////////////////////////////
-Route::Response
-LineRoute::impl(HttpPost, const Request& req, int pid, int p, int lid, int tid)
-  const
-{
+Route::Response LineRoute::impl(HttpPost, const Request &req, int pid, int p,
+                                int lid, int tid) const {
   CROW_LOG_INFO << "(LineRoute) POST token: " << pid << ":" << p << ":" << lid
                 << ":" << tid;
   const auto len = get<int>(req.url_params, "len");
@@ -133,7 +120,7 @@ LineRoute::impl(HttpPost, const Request& req, int pid, int p, int lid, int tid)
     THROW(BadRequest, "(LineRoute) missing correction data");
   }
   auto conn = must_get_connection();
-  DbLine line{ pid, p, lid };
+  DbLine line{pid, p, lid};
   if (not line.load(conn)) {
     THROW(NotFound, "(LineRoute) cannot find ", pid, ":", p, ":", lid);
   }
@@ -145,51 +132,43 @@ LineRoute::impl(HttpPost, const Request& req, int pid, int p, int lid, int tid)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void
-LineRoute::correct(DbLine& line, const std::string& correction)
-{
+void LineRoute::correct(DbLine &line, const std::string &correction) {
   WagnerFischer wf;
-  const auto slice = line.slice();
   wf.set_gt(correction);
-  wf.set_ocr(slice.cor());
+  wf.set_ocr(line.slice().wocr());
   const auto lev = wf();
   CROW_LOG_INFO << "(LineRoute) correction: " << correction;
-  CROW_LOG_INFO << "(LineRoute) line.ocr(): " << slice.ocr();
-  CROW_LOG_INFO << "(LineRoute) line.cor(): " << slice.cor();
+  CROW_LOG_INFO << "(LineRoute) line.ocr(): " << line.slice().ocr();
+  CROW_LOG_INFO << "(LineRoute) line.cor(): " << line.slice().cor();
 
   // correct
   wf.correct(line);
 
-  CROW_LOG_INFO << "(LineRoute) line.cor(): " << slice.cor();
-  CROW_LOG_INFO << "(LineRoute)        lev: " << lev;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-void
-LineRoute::correct(DbLine& line, const std::string& correction, int b, int len)
-{
-  std::cerr << "(LineRoute::correct) b = " << b << " len = " << len << "\n";
-  WagnerFischer wf;
-  const auto slice = line.slice();
-  wf.set_gt(correction);
-  wf.set_ocr(slice.cor());
-  const auto lev = wf(b, len);
-  CROW_LOG_INFO << "(LineRoute) correction: " << correction;
-  CROW_LOG_INFO << "(LineRoute) line.ocr(): " << slice.ocr();
-  CROW_LOG_INFO << "(LineRoute) line.cor(): " << slice.cor();
-
-  // correct
-  wf.correct(line, b, correction.size());
-
-  // DO NOT RE-USE slice var
   CROW_LOG_INFO << "(LineRoute) line.cor(): " << line.slice().cor();
   CROW_LOG_INFO << "(LineRoute)        lev: " << lev;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void
-LineRoute::update(MysqlConnection& mysql, const DbLine& line)
-{
+void LineRoute::correct(DbLine &line, const std::string &correction, int b,
+                        int len) {
+  std::cerr << "(LineRoute::correct) b = " << b << " len = " << len << "\n";
+  WagnerFischer wf;
+  wf.set_gt(correction);
+  wf.set_ocr(line.slice().wocr());
+  const auto lev = wf(b, len);
+  CROW_LOG_INFO << "(LineRoute) correction: " << correction;
+  CROW_LOG_INFO << "(LineRoute) line.ocr(): " << line.slice().ocr();
+  CROW_LOG_INFO << "(LineRoute) line.cor(): " << line.slice().cor();
+
+  // correct
+  wf.correct(line, b, correction.size());
+
+  CROW_LOG_INFO << "(LineRoute) line.cor(): " << line.slice().cor();
+  CROW_LOG_INFO << "(LineRoute)        lev: " << lev;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void LineRoute::update(MysqlConnection &mysql, const DbLine &line) {
   using namespace sqlpp;
   tables::Contents contents;
   MysqlCommitter committer(mysql);
@@ -198,15 +177,13 @@ LineRoute::update(MysqlConnection& mysql, const DbLine& line)
                                          contents.pageid == line.pageid and
                                          contents.lineid == line.lineid));
   // add new contents
-  auto stmnt = mysql.db().prepare(
-    insert_into(contents).set(contents.bookid = line.bookid,
-                              contents.pageid = line.pageid,
-                              contents.lineid = line.lineid,
-                              contents.seq = parameter(contents.seq),
-                              contents.ocr = parameter(contents.ocr),
-                              contents.cor = parameter(contents.cor),
-                              contents.cut = parameter(contents.cut),
-                              contents.conf = parameter(contents.conf)));
+  auto stmnt = mysql.db().prepare(insert_into(contents).set(
+      contents.bookid = line.bookid, contents.pageid = line.pageid,
+      contents.lineid = line.lineid, contents.seq = parameter(contents.seq),
+      contents.ocr = parameter(contents.ocr),
+      contents.cor = parameter(contents.cor),
+      contents.cut = parameter(contents.cut),
+      contents.conf = parameter(contents.conf)));
   for (auto i = 0U; i < line.line.size(); i++) {
     stmnt.params.seq = i;
     stmnt.params.ocr = int(line.line[i].ocr);
