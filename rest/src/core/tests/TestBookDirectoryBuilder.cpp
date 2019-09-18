d9b7facccd2399acc00a708ef40062029b65910b
finkf <finkf@cis.lmu.de>
2019-09-18 09:51:29 +0200

    Fix tests for page ids

--- rest/src/core/tests/TestBookDirectoryBuilder.cpp
+++ rest/src/core/tests/TestBookDirectoryBuilder.cpp
@@ -11,32 +11,26 @@
 
 using namespace pcw;
 
-struct BookDirectoryBuilderFixture
-{
+struct BookDirectoryBuilderFixture {
   TmpDir tmp;
   BookDirectoryBuilder builder;
-  BookDirectoryBuilderFixture()
-    : tmp()
-    , builder(tmp.dir(), "test")
-  {}
+  BookDirectoryBuilderFixture() : tmp(), builder(tmp.dir(), "test") {}
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 BOOST_FIXTURE_TEST_SUITE(BookDirectoryBuilder, BookDirectoryBuilderFixture)
 
 ////////////////////////////////////////////////////////////////////////////////
-BOOST_AUTO_TEST_CASE(BuildAbbyyTest)
-{
+BOOST_AUTO_TEST_CASE(BuildAbbyyTest) {
   builder.add_zip_file_path(
-    "misc/data/test/1841-DieGrenzboten-abbyy-small.zip");
+      "misc/data/test/1841-DieGrenzboten-abbyy-small.zip");
   const auto book = builder.build();
   BOOST_REQUIRE(book);
   BOOST_REQUIRE(book->size() == 2);
-  for (const auto& page : *book) {
-    std::cout << "PAGE ID: " << page->id() << "\n";
-  }
-  BOOST_CHECK(book->find(1) != nullptr);
-  BOOST_CHECK(book->find(2) != nullptr);
+  BOOST_CHECK(book->find(179392) != nullptr);
+  BOOST_CHECK(book->find(179393) != nullptr);
+  BOOST_CHECK_EQUAL(book->find(179392)->id(), 179392);
+  BOOST_CHECK_EQUAL(book->find(179393)->id(), 179393);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
